53185f6431774537122d0c02ccdaf420aeb5d2a1
vaibhavdostuff <vaibhavnegi141@gmail.com>
2023-12-30 23:38:52 +0530

    print array and reverse array

--- .py
+++ .py
@@ -4,4 +4,9 @@ arr = [1, 2, 3, 4, 5];
 print("Elements of given array: ");     
     
 for i in range(0, len(arr)):    
-    print(arr[i]),   
+    print(arr[i]),
+ 
+print("Array in reverse order: ");    
+  
+for i in range(len(arr)-1, -1, -1):     
+    print(arr[i]),    
