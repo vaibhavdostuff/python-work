951dba61835b9a6efe5006516eacac9075caed62
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-01 18:05:47 +0530

    issue

--- .py
+++ .py
@@ -1,20 +1,4 @@
-# print("this is my python programming space")
-arr = [1, 2, 3, 4, 5];     
-     
-print("Elements of given array: ");     
-    
-for i in range(0, len(arr)):    
-    print(arr[i]),
+a = [1, 2, 3, 4, 5]
  
-print("Array in reverse order: ");    
-  
-for i in range(len(arr)-1, -1, -1):     
-    print(arr[i]),    
-
-    
-sum = 0;    
-         
-for i in range(0, len(arr)):    
-   sum = sum + arr[i];    
-     
-print("Sum of all the elements of an array: " + str(sum));    
+for x in range(len(a)):
+    print [x] 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # this is my python programming space
+# some technical problem 
