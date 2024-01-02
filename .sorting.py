d4144d5faf32de9fbab954e2b1e601485fece5f2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-02 19:13:31 +0530

    sorting program reverse

--- .sorting.py
+++ .sorting.py
@@ -1,3 +1,7 @@
+#list = [10, 25 , 30 , 7, 77]
+#list.sort()
+#print("Now it is sorted:", list)
+
 list = [10, 25 , 30 , 7, 77]
-list.sort()
-print("Now it is sorted:", list)
+list.sort(reverse=True)
+print(list)
