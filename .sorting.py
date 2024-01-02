46c7c4ade5704345a6cd6e2b525a99acb6720177
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-02 22:25:08 +0530

    sort programms

--- .sorting.py
+++ .sorting.py
@@ -1,24 +1,32 @@
-#list = [10, 25 , 30 , 7, 77]
-#list.sort()
-#print("Now it is sorted:", list)
+#1
 
-#list = [10, 25 , 30 , 7, 77]
-#list.sort(reverse=True)
-#print(list)
+list = [10, 25 , 30 , 7, 77]
+list.sort()
+print("Now it is sorted:", list)
 
-#words = ["apple", "mango" , "banana" , "orange" , "grape"]
-#words.sort(key=len)
-#print("Sorted by Length:", words)
+#2
 
-#words = ["apple", "mango", "banana", "orange", "grape"]
-#words.sort(key=len, reverse=True)
-#print("Sorted by Length (in reverse):", words)
+list = [10, 25 , 30 , 7, 77]
+list.sort(reverse=True)
+print(list)
 
+#3
+words = ["apple", "mango" , "banana" , "orange" , "grape"]
+words.sort(key=len)
+print("Sorted by Length:", words)
 
-#people = [("vaibhav", 25), ("sriansh", 30), ("aakansha", 22), ("priyanka", 28)]
-#people.sort(key=lambda x: x[1])
-#print("Sorted by Age:", people)
+#4
+words = ["apple", "mango", "banana", "orange", "grape"]
+words.sort(key=len, reverse=True)
+print("Sorted by Length (in reverse):", words)
 
+#5
+
+people = [("vaibhav", 25), ("sriansh", 30), ("aakansha", 22), ("priyanka", 28)]
+people.sort(key=lambda x: x[1])
+print("Sorted by Age:", people)
+
+#6
 
 people = [("vaibhav", 25), ("sriansh", 30), ("aakansha", 22), ("priyanka", 28)]
 people.sort(key=lambda x: x[1], reverse=True)
