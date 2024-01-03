84ab52df07441883d07beb1e9404e068895a585a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-03 13:32:44 +0530

    python pattern5 program

--- pattern.py
+++ pattern.py
@@ -28,3 +28,11 @@ for i in range(1, rows + 1):
         print(num, end=' ')
     print("\r")
 
+rows = 5
+num = rows
+# reverse for loop
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print(num, end=' ')
+    print("\r")
+
