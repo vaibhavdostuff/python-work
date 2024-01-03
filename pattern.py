b5c4e1575829fe93afa1f2d6ab261607c864e87f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-03 13:42:31 +0530

    python pattern8 program

--- pattern.py
+++ pattern.py
@@ -48,3 +48,10 @@ for i in range(1, rows + 1):
         print(j, end=' ')
     print("\r")
 
+rows = 7
+for i in range(rows, 0, -1):
+    num = i
+    for j in range(0, i):
+        print(num, end=' ')
+    print("\r")
+
