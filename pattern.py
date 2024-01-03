4ce705ea0211bed268c1a871ec34a3b7109cef15
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-03 13:35:31 +0530

    python pattern6 program

--- pattern.py
+++ pattern.py
@@ -21,18 +21,24 @@ for i in range(rows, 0, -1):
         print(b, end=' ')
     print('\r')
 
-rows = 5
+rows = 7
 num = rows
 for i in range(1, rows + 1):
     for j in range(1, i + 1):
         print(num, end=' ')
     print("\r")
 
-rows = 5
+rows = 7
 num = rows
 # reverse for loop
 for i in range(rows, 0, -1):
     for j in range(0, i):
         print(num, end=' ')
+    print("\r")
+
+rows = 7
+for i in range(rows, 0, -1):
+    for j in range(0, i + 1):
+        print(j, end=' ')
     print("\r")
 
