a92c85d1ccd963d6830d478ec44b461044f44185
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-03 13:48:54 +0530

    python pattern program

--- pattern.py
+++ pattern.py
@@ -21,12 +21,14 @@ for i in range(rows, 0, -1):
         print(b, end=' ')
     print('\r')
 
+
 rows = 7
 num = rows
 for i in range(1, rows + 1):
     for j in range(1, i + 1):
         print(num, end=' ')
     print("\r")
+
 
 rows = 7
 num = rows
@@ -36,11 +38,13 @@ for i in range(rows, 0, -1):
         print(num, end=' ')
     print("\r")
 
+
 rows = 7
 for i in range(rows, 0, -1):
     for j in range(0, i + 1):
         print(j, end=' ')
     print("\r")
+
 
 rows = 7
 for i in range(1, rows + 1):
@@ -48,10 +52,22 @@ for i in range(1, rows + 1):
         print(j, end=' ')
     print("\r")
 
+
 rows = 7
 for i in range(rows, 0, -1):
     num = i
     for j in range(0, i):
         print(num, end=' ')
     print("\r")
+
+
+rows = 4
+i = 1
+while i <= rows:
+    j = 1
+    while j <= i:
+        print((i * 2 - 1), end=" ")
+        j = j + 1
+    i = i + 1
+    print('')
 
