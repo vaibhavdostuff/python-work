bf9681049630fce7cb1a7fa331144ca7fb3eb35c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-03 13:56:41 +0530

    python pattern program

--- pattern.py
+++ pattern.py
@@ -71,3 +71,16 @@ while i <= rows:
     i = i + 1
     print('')
 
+
+start = 1
+stop = 2
+current_num = stop
+for row in range(1, 5):
+    for col in range(start, stop):
+        current_num -= 1
+        print(current_num, end=' ')
+    print("")
+    start = stop
+    stop += row
+    current_num = stop
+
