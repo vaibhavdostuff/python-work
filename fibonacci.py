f6805298174915cc11405d20ff45537db91d8f03
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-06 23:56:47 +0530

    fibonacci programm

--- fibonacci.py
+++ fibonacci.py
@@ -88,3 +88,29 @@ def fibonacci(n):
 
 # Driver Program
 print(fibonacci(9))
+
+#5
+from functools import lru_cache
+
+# Function for nth Fibonacci number
+
+@lru_cache(None)
+def fibonacci(num: int) -> int:
+
+	# check if num is less than 0
+	# it will return none
+	if num < 0:
+		print("Incorrect input")
+		return
+
+	# check if num between 1, 0
+	# it will return num
+	elif num < 2:
+		return num
+
+	# return the fibonacci of num - 1 & num - 2
+	return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+# Driver Program
+print(fibonacci(9))
