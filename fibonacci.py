782394928b744b2fea3ad0b0ea144d7a8dad7911
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-06 23:57:24 +0530

    fibonacci programm

--- fibonacci.py
+++ fibonacci.py
@@ -114,3 +114,18 @@ def fibonacci(num: int) -> int:
 
 # Driver Program
 print(fibonacci(9))
+
+#6
+def fibonacci(n, memo={}):
+	if n <= 0:
+		return 0
+	elif n == 1:
+		return 1
+	elif n in memo:
+		return memo[n]
+	else:
+		memo[n] = fibonacci(n-1) + fibonacci(n-2)
+		return memo[n]
+
+# Driver Program
+print(fibonacci(9))
