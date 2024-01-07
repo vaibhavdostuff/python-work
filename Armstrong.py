9ab9e3973fd5efb3af2ba08b5b89f5684895a0c2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-07 23:42:33 +0530

    Armstrong 3programm

--- Armstrong.py
+++ Armstrong.py
@@ -66,3 +66,18 @@ else:
 	print("The given number", s, "is not armstrong number")
 
 # This code is contributed by Gangarajula Laxmi
+
+#3
+def is_armstrong(num):
+	num_str = str(num)
+	n = len(num_str)
+	sum = 0
+	for digit in num_str:
+		sum += int(digit)**n
+	if sum == num:
+		return True
+	else:
+		return False
+num=153
+print(is_armstrong(num))
+
