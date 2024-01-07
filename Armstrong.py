75accc695469bb3270d8c511ea617116170de663
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-07 23:41:47 +0530

    Armstrong 2programm

--- Armstrong.py
+++ Armstrong.py
@@ -1,3 +1,4 @@
+#1
 # Python program to determine whether
 # the number is Armstrong number or not
  
@@ -45,3 +46,23 @@ print(isArmstrong(x))
  
 x = 1253
 print(isArmstrong(x))
+
+#2
+# python 3 program
+# to check whether the given number is armstrong or not
+# without using power function
+
+n = 153 # or n=int(input()) -> taking input from user
+s = n # assigning input value to the s variable
+b = len(str(n))
+sum1 = 0
+while n != 0:
+	r = n % 10
+	sum1 = sum1+(r**b)
+	n = n//10
+if s == sum1:
+	print("The given number", s, "is armstrong number")
+else:
+	print("The given number", s, "is not armstrong number")
+
+# This code is contributed by Gangarajula Laxmi
