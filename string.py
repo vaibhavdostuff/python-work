5e458d0bb63561a800325c104310d32fd6bef671
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-08 20:00:27 +0530

    string program

--- string.py
+++ string.py
@@ -1,1 +1,21 @@
+#1
 print("first string program python") 
+
+#2 
+String1 = '2 string program python'
+print(String1) 
+
+String1 = "vaibhav here"
+print("\nfirst string program python") 
+print(String1) 
+
+String1 = '''vaibhav here'''
+print("\nfirst string program python") 
+print(String1) 
+
+String1 = '''vaibhav
+is
+here'''
+print("\nfirst string program python") 
+print(String1) 
+
