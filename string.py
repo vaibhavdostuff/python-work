6b6cc7fb4c923d142e1092036708656dabf43cda
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-08 20:02:15 +0530

    string program

--- string.py
+++ string.py
@@ -19,3 +19,13 @@ here'''
 print("\nfirst string program python") 
 print(String1) 
 
+#3
+String1 = "vaibhav is here"
+print("starting String: ") 
+print(String1) 
+
+print("\nFirst character of String is: ") 
+print(String1[0]) 
+
+print("\nLast character of String is: ") 
+print(String1[-1]) 
