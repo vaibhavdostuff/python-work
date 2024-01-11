cd0114cd23610e74ae861ac4c6e9cb994e8f07c0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-11 18:47:35 +0530

    string program

--- string.py
+++ string.py
@@ -199,3 +199,14 @@ print(String1)
 String1 = "\n{0:^16} was started in {1:<4}!".format("vaibhav is here", 
                                                     2023) 
 print(String1) 
+
+#18
+# Python Program for 
+# Old Style Formatting 
+# of Integers 
+  
+Integer1 = 12.3456789
+print("Formatting in 3.2f format: ") 
+print('The value of Integer1 is %3.2f' % Integer1) 
+print("\nFormatting in 3.4f format: ") 
+print('The value of Integer1 is %3.4f' % Integer1) 
