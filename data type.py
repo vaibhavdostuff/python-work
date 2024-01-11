990229dd8852ac7f2eb181f4855852702daa44cc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-11 20:03:42 +0530

    data type programm

---
+++ data type.py
@@ -0,0 +1,15 @@
+x = "Hello World"
+x = 50
+x = 60.5
+x = 3j
+x = ["geeks", "for", "geeks"] 
+x = ("geeks", "for", "geeks") 
+x = range(10)  
+x = {"name": "Suraj", "age": 24} 
+x = {"geeks", "for", "geeks"} 
+x = frozenset({"geeks", "for", "geeks"}) 
+x = True
+x = b"Geeks"
+x = bytearray(4) 
+x = memoryview(bytes(6)) 
+x = None
