e3ac4145442c5cb1146877dabc577cdf32e76b1f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-15 21:05:00 +0530

    fun programm

--- .py
+++ .py
@@ -238,3 +238,21 @@ while True:
         break
     else:
         print("Invalid choice.")
+
+#18
+def count_word_frequency(text):
+    words = text.split()
+    word_frequency = {}
+    for word in words:
+        word = word.lower()
+        if word in word_frequency:
+            word_frequency[word] += 1
+        else:
+            word_frequency[word] = 1
+    return word_frequency
+
+input_text = input("Enter a text: ")
+frequencies = count_word_frequency(input_text)
+for word, frequency in frequencies.items():
+    print(f"{word}: {frequency} times")
+
