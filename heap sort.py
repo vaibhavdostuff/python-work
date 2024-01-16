497d77b5f731c0f7aafef96fd893d45f6a9941cd
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-16 23:51:17 +0530

    heap sort programm

--- heap sort.py
+++ heap sort.py
@@ -60,4 +60,20 @@ print('Sorted array is')
 for i in range(n):
 	print(arr[i])
 
-# This code is contributed by Mohit Kumra
+#2
+import heapq
+
+# Function to perform the sorting using
+# heaop sort
+def heap_sort(arr):
+	heapq.heapify(arr)
+	result = []
+	while arr:
+		result.append(heapq.heappop(arr))
+	return result
+
+# Driver Code
+arr = [60, 20, 40, 70, 30, 10]
+print("Input Array: ", arr)
+print("Sorted Array: ", heap_sort(arr))
+
