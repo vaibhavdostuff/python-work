1cd14d810b0295cd0348b3b63759557aa90c60e6
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-16 23:36:56 +0530

    diff programm

--- diff programm.py
+++ diff programm.py
@@ -14,3 +14,20 @@ if check_year(year):
     print("Leap Year")
 else:
     print("Not a Leap Year")
+
+#2
+def checkYear(year): 
+	
+	# Return true if year is a multiple 
+	# of 4 and not multiple of 100. 
+	# OR year is multiple of 400. 
+	import calendar
+	return(calendar.isleap(year)) 
+	
+# Driver Code 
+year = 2000
+if (checkYear(year)): 
+	print("Leap Year") 
+else: 
+	print("Not a Leap Year") 
+
