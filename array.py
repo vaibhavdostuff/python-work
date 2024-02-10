53480a4d84c7ffbcaca79c2ea2552c4d01b369e8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-10 13:07:02 +0530

    array python programm

--- array.py
+++ array.py
@@ -47,4 +47,34 @@ ans = sum(arr)
 # display sum
 print('Sum of the array is ', ans)
 
+#3
+from functools import reduce
+# Python 3 code to find sum
+# of elements in given array
+
+
+def _sum(arr):
+
+	# iterate over array
+	# using reduce and get 
+	# sum on accumulator
+	sum = reduce(lambda a, b: a+b, arr) 
+
+	return(sum)
+
+
+# driver function
+arr = []
+# input values to list
+arr = [12, 3, 4, 15]
+
+# calculating length of array
+n = len(arr)
+
+ans = _sum(arr)
+
+# display sum
+print('Sum of the array is ', ans)
+
+
 
