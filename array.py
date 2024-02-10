6cb77784a213cf9517527850cf8c98d20f949b0f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-10 13:10:20 +0530

    array python programm

--- array.py
+++ array.py
@@ -136,3 +136,16 @@ arr = [10, 324, 45, 90, 9808]
 n = len(arr)
 Ans = largest(arr, n)
 print("Largest in given array ", Ans)
+
+#8
+# Python3 program to find maximum
+# in arr[] of size n
+def largest(arr, n):
+	ans = max(arr)
+	return ans;
+
+# Driver code
+if __name__ == '__main__':
+	arr = [10, 324, 45, 90, 9808]
+	n = len(arr)
+	print ("Largest in given array ", largest(arr, n))
