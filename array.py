64eaccb4c6b1ae2420d5f5b3a07871b7abd313a5
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-10 13:11:19 +0530

    array python programm

--- array.py
+++ array.py
@@ -170,3 +170,21 @@ arr = [10, 324, 45, 90, 9808]
 n = len(arr)
 Ans = largest(arr)
 print("Largest in given array ", Ans)
+
+#10
+# python program to find large number in the given array
+import operator
+# Initializing the list
+arr = [2, 1, 7, 3, 0]
+max=0
+
+# printing the original list
+print('The given array is:', arr)
+
+#checking for large element
+for i in arr:
+if operator.gt(i,max):
+	max=i
+
+# printing the large number in the array
+print('The biggest number in the given array is:', max)
