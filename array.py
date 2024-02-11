fadfdc637a84680d0bce5613ca97ecd2580c6c54
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-11 23:02:53 +0530

    array python programm

--- array.py
+++ array.py
@@ -259,3 +259,12 @@ arr = [1, 2, 3, 4, 5, 6, 7]
 leftRotate(arr, 2, 7)
 printArray(arr, 7)
 
+#14
+# Function to reverse arr[]
+def rverseArray(arr,d):
+	c=(arr[d:])+(arr[:d])
+	return c
+# Driver function to test above functions
+arr = [1, 2, 3, 4, 5, 6, 7]
+d=2
+print(rverseArray(arr,d))
