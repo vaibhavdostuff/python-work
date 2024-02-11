15a27cc750e7fb687ae102fc5575262093f0b69d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-11 23:03:55 +0530

    array python programm

--- array.py
+++ array.py
@@ -296,3 +296,20 @@ arr = [1, 2, 3, 4, 5, 6, 7]
 leftRotate(arr, 2) # Rotate array by 2
 printArray(arr)
 
+#16
+# Python program to split array and move first
+# part to end.
+
+
+def splitArr(a, n, k):
+	b = a[:k]
+	return (a[k::]+b[::])
+
+
+# main
+arr = [12, 10, 5, 6, 52, 36]
+n = len(arr)
+position = 2
+arr = splitArr(arr, n, position)
+for i in range(0, n):
+	print(arr[i], end=' ')
