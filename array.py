f4a161b14656b8a90ee1a9d8fd4a0bf8e4cabf4c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-11 23:04:14 +0530

    array python programm

--- array.py
+++ array.py
@@ -313,3 +313,16 @@ position = 2
 arr = splitArr(arr, n, position)
 for i in range(0, n):
 	print(arr[i], end=' ')
+
+#17
+# Python program to split array and move first
+# part to end.
+
+arr = [12, 10, 5, 6, 52, 36]
+n = len(arr)
+position = 2
+x = arr[:position]
+y = arr[position:]
+y.extend(x)
+for i in y:
+	print(i, end=" ")
