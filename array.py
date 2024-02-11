f1e842cf9e0ad000e9c71e4421933161758ec1e6
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-11 23:04:52 +0530

    array python programm

--- array.py
+++ array.py
@@ -326,3 +326,15 @@ y = arr[position:]
 y.extend(x)
 for i in y:
 	print(i, end=" ")
+
+#18
+def split_and_add(arr, n):
+	return [arr[(i + n) % len(arr)] for i in range(len(arr))]
+
+
+arr = [12, 10, 5, 6, 52, 36]
+n = 2
+
+result = split_and_add(arr, n)
+
+print(*result)
