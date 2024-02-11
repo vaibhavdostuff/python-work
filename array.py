e9378d5f10d05dacd54a86310eaf397f428693d6
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-11 23:05:36 +0530

    array python programm

--- array.py
+++ array.py
@@ -338,3 +338,17 @@ n = 2
 result = split_and_add(arr, n)
 
 print(*result)
+
+#19
+# This code finds the remainder of the product of all the elements in the array arr divided by 'n'.
+def findremainder(arr, len, n):
+	product = 1
+	for i in range(len):
+		product = product * arr[i]
+	return product % n
+
+
+arr = [100, 10, 5, 25, 35, 14]
+len = len(arr)
+n = 11
+print(findremainder(arr, len, n))
