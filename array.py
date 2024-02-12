5b98e652ef192709efeffc983b7f9a7769a2f068
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-12 18:42:00 +0530

    array python programm

--- array.py
+++ array.py
@@ -352,3 +352,21 @@ arr = [100, 10, 5, 25, 35, 14]
 len = len(arr)
 n = 11
 print(findremainder(arr, len, n))
+
+#20
+from functools import reduce
+
+def remainderAfterMultiplication(arr, n):
+	result = reduce(lambda x, y: (x * y) % n, arr)
+	return result
+
+# Driver Code
+arr1 = [100, 10, 5, 25, 35, 14]
+n1 = 11
+result1 = remainderAfterMultiplication(arr1, n1)
+print(result1)
+
+arr2 = [100, 10]
+n2 = 5
+result2 = remainderAfterMultiplication(arr2, n2)
+print(result2)
