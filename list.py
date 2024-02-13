792488684933e8b5153f9f5ebe6237d040e18824
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-13 18:59:15 +0530

    list python programm

--- list.py
+++ list.py
@@ -80,3 +80,22 @@ def swapList(list):
 newList = [12, 35, 9, 56, 24]
 
 print(swapList(newList))
+
+#6
+def swap_first_last_3(lst):
+	# Check if list has at least 2 elements
+	if len(lst) >= 2:
+		# Swap the first and last elements using slicing
+		lst = lst[-1:] + lst[1:-1] + lst[:1]
+	return lst
+
+# Initializing the input
+inp=[12, 35, 9, 56, 24]
+
+# Printing the original input
+print("The original input is:",inp)
+
+result=swap_first_last_3(inp)
+
+# Printing the result
+print("The output after swap first and last is:",result)
