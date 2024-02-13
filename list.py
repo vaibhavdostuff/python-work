3a64863a85eb622fe1e66f3a097b036b3f41a33c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-13 18:59:48 +0530

    list python programm

--- list.py
+++ list.py
@@ -99,3 +99,20 @@ result=swap_first_last_3(inp)
 
 # Printing the result
 print("The output after swap first and last is:",result)
+
+#10
+# Python3 program to swap elements
+# at given positions
+
+# Swap function
+def swapPositions(list, pos1, pos2):
+	
+	list[pos1], list[pos2] = list[pos2], list[pos1]
+	return list
+
+# Driver function
+List = [23, 65, 19, 90]
+pos1, pos2 = 1, 3
+
+print(swapPositions(List, pos1-1, pos2-1))
+
