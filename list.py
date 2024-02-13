937b330a348f833accd13aba1860addb08436bdc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-13 19:01:54 +0530

    list python programm

--- list.py
+++ list.py
@@ -201,3 +201,22 @@ for i in test_list:
 
 # Printing length of list
 print("Length of list using naive method is : " + str(counter))
+
+#16
+from operator import length_hint
+
+# Initializing list
+test_list = [1, 4, 5, 7, 8]
+
+# Printing test_list
+print("The list is : " + str(test_list))
+
+# Finding length of list using len()
+list_len = len(test_list)
+
+# Finding length of list using length_hint()
+list_len_hint = length_hint(test_list)
+
+# Printing length of list
+print("Length of list using len() is : " + str(list_len))
+print("Length of list using length_hint() is : " + str(list_len_hint))
