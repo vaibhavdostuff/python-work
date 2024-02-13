74373ffa031ceeb05db67ecce2a2cc31d14bf93b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-13 19:01:39 +0530

    list python programm

--- list.py
+++ list.py
@@ -183,3 +183,21 @@ print(swapPositions(List, pos1-1, pos2-1))
 li = [10, 20, 30]
 n = len(li)
 print("The length of list is: ", n)
+
+#15
+# Initializing list
+test_list = [1, 4, 5, 7, 8]
+
+# Printing test_list
+print("The list is : " + str(test_list))
+
+# Finding length of list using loop
+# Initializing counter
+counter = 0
+for i in test_list:
+
+	# incrementing counter
+	counter = counter + 1
+
+# Printing length of list
+print("Length of list using naive method is : " + str(counter))
