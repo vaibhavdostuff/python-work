8116d54c1e103bb91e5ab1729573a782c5b1b3fe
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-13 19:02:08 +0530

    list python programm

--- list.py
+++ list.py
@@ -220,3 +220,19 @@ list_len_hint = length_hint(test_list)
 # Printing length of list
 print("Length of list using len() is : " + str(list_len))
 print("Length of list using length_hint() is : " + str(list_len_hint))
+
+#17
+# Initializing list
+test_list = [1, 4, 5, 7, 8]
+
+# Printing test_list
+print("The list is : " + str(test_list))
+
+# Finding length of list
+# using sum()
+list_len = sum(1 for i in test_list)
+
+
+# Printing length of list
+print("Length of list using len() is : " + str(list_len))
+print("Length of list using length_hint() is : " + str(list_len))
