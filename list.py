dd14a9e2fbbf2a7bc172c99314756a1b11a713fe
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-14 18:41:12 +0530

    list python programm

--- list.py
+++ list.py
@@ -366,3 +366,9 @@ for i in test_list:
 	if(i == 4):
 		print("Element Exists")
 	
+#26
+# Initializing list
+test_list = [1, 6, 3, 5, 3, 4]
+
+result = any(item in test_list for item in test_list)
+print("Does string contain any list element : " +str(bool(result)))
