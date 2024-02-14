832e3c36bb9b1bd87e09ad8b72549cc65d3d7596
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-14 18:40:51 +0530

    list python programm

--- list.py
+++ list.py
@@ -357,3 +357,12 @@ if i in lst:
 else: 
 	print("not exist")
 
+#25
+# Initializing list
+test_list = [1, 6, 3, 5, 3, 4]
+
+# Checking if 4 exists in list
+for i in test_list:
+	if(i == 4):
+		print("Element Exists")
+	
