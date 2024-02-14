43ba111bd3a7dbc2da22586828be9077f85c5578
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-14 18:39:08 +0530

    list python programm

--- list.py
+++ list.py
@@ -283,3 +283,18 @@ length = sum(1 for _ in test_list)
 
 # Print the length of the list
 print("Length of list using list comprehension is:", length)
+
+#21
+# Define a function to count the number of elements in a list using recursion
+def count_elements_recursion(lst):
+	# Base case: if the list is empty, return 0
+	if not lst:
+		return 0
+	# Recursive case: add 1 to the count of the remaining elements in the list
+	return 1 + count_elements_recursion(lst[1:])
+
+
+# Test the function with a sample list
+lst = [1, 2, 3, 4, 5]
+print("The length of the list is:", count_elements_recursion(lst))
+
