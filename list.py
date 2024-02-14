5974ab45f397b718478d04bcae47f359109d3faf
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-14 18:38:35 +0530

    list python programm

--- list.py
+++ list.py
@@ -271,3 +271,15 @@ list_len = sum(1 for i in test_list)
 # Printing length of list
 print("Length of list using len() is : " + str(list_len))
 print("Length of list using length_hint() is : " + str(list_len))
+
+#20
+# Define the list to be used for the demonstration
+test_list = [1, 4, 5, 7, 8]
+
+# Calculate the length of the list using a list comprehension and the sum function
+# The list comprehension generates a sequence of ones for each element in the list
+# The sum function then sums all the ones to give the length of the list
+length = sum(1 for _ in test_list)
+
+# Print the length of the list
+print("Length of list using list comprehension is:", length)
