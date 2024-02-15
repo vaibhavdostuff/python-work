169c6194652c44d4c94f07fe6fdc8baed3d7c3e8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-15 13:00:11 +0530

    list python programm

--- list.py
+++ list.py
@@ -387,3 +387,28 @@ if exist_count > 0:
 	print("Yes, 15 exists in list")
 else:
 	print("No, 15 does not exists in list")
+
+#28
+from bisect import bisect_left ,bisect
+
+# Initializing list 
+test_list_set = [ 1, 6, 3, 5, 3, 4 ]
+test_list_bisect = [ 1, 6, 3, 5, 3, 4 ]
+
+print("Checking if 4 exists in list ( using set() + in) : ")
+
+# Checking if 4 exists in list 
+# using set() + in
+test_list_set = set(test_list_set)
+if 4 in test_list_set :
+	print ("Element Exists")
+
+print("Checking if 4 exists in list ( using sort() + bisect_left() ) : ")
+
+# Checking if 4 exists in list 
+# using sort() + bisect_left()
+test_list_bisect.sort()
+if bisect_left(test_list_bisect, 4)!=bisect(test_list_bisect, 4):
+	print ("Element Exists")
+else:
+	print("Element doesnt exist")
