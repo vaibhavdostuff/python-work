1ba11eaf8329108b0df368e3e1b2fe9382bad91a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-15 13:03:42 +0530

    list python programm

--- list.py
+++ list.py
@@ -449,3 +449,16 @@ print('word before clear:', word)
 word.clear()
 print('word after clear:', word)
 
+#32
+list1 = [1, 2, 3]
+
+# Printing list2 before deleting
+print("List1 before deleting is : "
+	+ str(list1))
+
+# deleting list using reinitialization
+list1 = []
+
+# Printing list2 after reinitialization
+print("List1 after clearing using reinitialization : "
+	+ str(list1))
