4b9658487fe09bf85aaec82bdf69e1cf33f9c888
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-15 13:03:59 +0530

    list python programm

--- list.py
+++ list.py
@@ -462,3 +462,16 @@ list1 = []
 # Printing list2 after reinitialization
 print("List1 after clearing using reinitialization : "
 	+ str(list1))
+
+#33
+# Initializing lists
+list1 = [1, 2, 3]
+
+# Printing list2 before deleting
+print("List1 before clearing is : "
+	+ str(list1))
+
+list1*=0
+# Printing list2 after reinitialization
+print("List1 after clearing using *=0 : "
+	+ str(list1))
