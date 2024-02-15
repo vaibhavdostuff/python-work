962ce3159e47e9c0e0be6792c0e18129f5d5c782
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-15 13:01:23 +0530

    list python programm

--- list.py
+++ list.py
@@ -427,3 +427,18 @@ else:
 	print("No, 15 does not exists in list")
 
 #30
+from collections import Counter
+
+test_list = [10, 15, 20, 7, 46, 2808]
+
+# Calculating frequencies
+frequency = Counter(test_list)
+
+# If the element has frequency greater than 0
+# then it exists else it doesn't exist
+if(frequency[15] > 0):
+	print("Yes, 15 exists in list")
+else:
+	print("No, 15 does not exists in list")
+
+#31
