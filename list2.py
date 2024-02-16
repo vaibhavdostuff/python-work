41c25123d342a64981deab0265901568b3708be0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-16 19:17:00 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -8,3 +8,10 @@ def Reverse(lst):
 lst = [10, 11, 12, 13, 14, 15]
 print(Reverse(lst))
 
+#2
+lst = [10, 11, 12, 13, 14, 15]
+lst.reverse()
+print("Using reverse() ", lst)
+
+print("Using reversed() ", list(reversed(lst)))
+
