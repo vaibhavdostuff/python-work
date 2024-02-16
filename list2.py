5959251569f7fc90695b6da80af17c7c0796a33f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-16 19:17:21 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -15,3 +15,21 @@ print("Using reverse() ", lst)
 
 print("Using reversed() ", list(reversed(lst)))
 
+#3
+# Reversing a list using two-pointer approach
+def reverse_list(arr):
+	left = 0
+	right = len(arr)-1
+	while (left < right):
+		# Swap
+		temp = arr[left]
+		arr[left] = arr[right]
+		arr[right] = temp
+		left += 1
+		right -= 1
+
+	return arr
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+print(reverse_list(arr))
+
