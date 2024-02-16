40d4e94001c1f496971a3d6a1639cd97f2777763
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-16 19:21:50 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -86,3 +86,20 @@ for ele in range(0, len(list1)):
 
 # printing total value
 print("Sum of all elements in given list: ", total)
+
+#8
+# Python program to find sum of elements in list
+total = 0
+ele = 0
+
+# creating a list
+list1 = [11, 5, 17, 18, 23] 
+
+# Iterate each element in list
+# and add them in variable total
+while(ele < len(list1)):
+	total = total + list1[ele]
+	ele += 1
+	
+# printing total value
+print("Sum of all elements in given list: ", total)
