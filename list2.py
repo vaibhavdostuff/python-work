06b55900001016a25278426da98231d853958843
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-16 19:17:53 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -46,3 +46,9 @@ for i in lst:
 	l.insert(0, i)
 # printing result
 print(l)
+
+#5
+original_list = [10, 11, 12, 13, 14, 15]
+new_list = [original_list[len(original_list) - i]
+			for i in range(1, len(original_list)+1)]
+print(new_list)
