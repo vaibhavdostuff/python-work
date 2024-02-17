fdead1445780224bc1956a9a833d5710cc4409a6
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-17 18:45:48 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -127,3 +127,13 @@ total = sumOfList(list1, len(list1))
 print("Sum of all elements in given list: ", total)
 
 #10
+# Python program to find sum of elements in list
+
+# creating a list
+list1 = [11, 5, 17, 18, 23]
+
+# using sum() function
+total = sum(list1)
+
+# printing total value
+print("Sum of all elements in given list: ", total)
