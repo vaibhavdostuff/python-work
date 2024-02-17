2386fea95796a7d091ae8ed711793247edeb27d7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-17 18:48:32 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -207,3 +207,15 @@ result1 = numpy.prod(list1)
 result2 = numpy.prod(list2)
 print(result1)
 print(result2)
+
+#18
+# Python3 program to multiply all values in the
+# list using lambda function and reduce()
+from functools import reduce
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+
+result1 = reduce((lambda x, y: x * y), list1)
+result2 = reduce((lambda x, y: x * y), list2)
+print(result1)
+print(result2)
