71bfb10e411a171b089a9ac6d0305ab4f2bf8610
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-17 18:46:22 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -137,3 +137,17 @@ total = sum(list1)
 
 # printing total value
 print("Sum of all elements in given list: ", total)
+
+#11
+# Python 3 program to find the sum of all elements in the
+# list using add function of operator module
+
+from operator import*
+list1 = [12, 15, 3, 10]
+result = 0
+for i in list1:
+# Adding elements in the list using
+# add function of operator module
+	result = add(i, result)
+# printing the result
+print(result)
