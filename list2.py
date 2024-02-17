6f69c1eb75e139bdbf4937f30f22e72d3e2b3a13
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-17 18:48:18 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -193,3 +193,17 @@ list1 = [1, 2, 3]
 list2 = [3, 2, 4]
 print(multiplyList(list1))
 print(multiplyList(list2))
+
+#17
+# Python3 program to multiply all values in the
+# list using numpy.prod()
+
+import numpy
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+
+# using numpy.prod() to get the multiplications
+result1 = numpy.prod(list1)
+result2 = numpy.prod(list2)
+print(result1)
+print(result2)
