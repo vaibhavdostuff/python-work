c7b4bdddc1018c7bc474c44de4b8f754c7c2a872
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-18 15:05:36 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -328,3 +328,13 @@ list1.sort()
 
 # printing the first element
 print("Smallest element is:", list1[0])
+
+#27
+# list of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# sorting the list
+list1.sort(reverse=True)
+
+# printing the first element
+print("Smallest element is:", list1[-1])
