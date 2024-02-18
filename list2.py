138d2769610ae3fb4d09abb0347ad5a36ac9a6cf
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-18 14:54:28 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -231,3 +231,17 @@ result1 = math.prod(list1)
 result2 = math.prod(list2)
 print(result1)
 print(result2)
+
+#20
+# Python 3 program to multiply all numbers in
+# the given list by importing operator module
+
+from operator import*
+list1 = [1, 2, 3]
+m = 1
+for i in list1:
+# multiplying all elements in the given list 
+# using mul function of operator module
+	m = mul(i, m)
+# printing the result
+print(m)
