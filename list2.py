72e123efcb28deb5ef28ff83a7183b99b4768328
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-18 14:54:55 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -245,3 +245,19 @@ for i in list1:
 	m = mul(i, m)
 # printing the result
 print(m)
+
+#21
+def multiply_list(list1):
+	# Initialize product to 1
+	product = 1
+	
+	# Iterate through each element in the list
+	for element in list1:
+		product *= element
+	
+	return product
+
+# Example
+list1 = [1, 2, 3, 4, 5]
+result = multiply_list(list1)
+print(result)
