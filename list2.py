f97b8141cca5b2000e34f6481212738aa3f9d654
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-19 23:29:59 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -369,3 +369,21 @@ for i in range(1, num + 1):
 print("Smallest element is:", min(list1))
 
 #30
+# Python program to find smallest 
+# number in a list
+
+l=[ int(l) for l in input("List:").split(",")]
+print("The list is ",l)
+
+# Assign first element as a minimum.
+min1 = l[0]
+
+for i in range(len(l)):
+
+	# If the other element is min than first element
+	if l[i] < min1: 
+		min1 = l[i] #It will change
+
+print("The smallest element in the list is ",min1)
+
+#31
