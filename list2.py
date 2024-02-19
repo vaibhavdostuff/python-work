7b368eaa671003964e1c20afda8656392f001578
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-19 23:34:31 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -444,3 +444,13 @@ list1.sort()
 print("Largest element is:", list1[-1])
 
 #37
+# Python program to find largest
+# number in a list
+
+# List of numbers
+list1 = [10, 20, 4, 45, 99]
+
+# Printing the maximum element
+print("Largest element is:", max(list1))
+
+#38
