36f1a6509f9cd9c67f3754be6c0a6d0849f24c28
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-19 23:29:26 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -349,3 +349,23 @@ list1 = [10, 20, 1, 45, 99]
 
 # printing the minimum element
 print("Smallest element is:", min(list1))
+
+#29
+# Python program to find smallest 
+# number in a list
+
+# creating empty list
+list1 = []
+
+# asking number of elements to put in list
+num = int(input("Enter number of elements in list: "))
+
+# iterating till num to append elements in list
+for i in range(1, num + 1):
+	ele= int(input("Enter elements: "))
+	list1.append(ele)
+	
+# print minimum element:
+print("Smallest element is:", min(list1))
+
+#30
