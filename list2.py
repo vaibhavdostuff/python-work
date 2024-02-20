8f160514263bd95a51d69bef21ea806f2fa9a3dc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-20 23:04:39 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -490,3 +490,28 @@ from functools import reduce
 lst = [20, 10, 20, 4, 100]
 largest_elem = reduce(max, lst)
 print(largest_elem)
+
+#41
+# Function to find the largest element in the list
+def FindLargest(itr, ele, list1):
+
+# Base condition
+	if itr == len(list1):
+		print("Largest element in the list is: ", ele)
+		return
+
+	# Check max condition
+	if ele < list1[itr]:
+		ele = list1[itr]
+
+		# Recursive solution
+	FindLargest(itr+1, ele, list1)
+
+	return
+
+# input list
+list1 = [2, 1, 7, 9, 5, 4]
+
+FindLargest(0, list1[0], list1)
+
+#42
