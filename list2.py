3d0bf2ed166ce4ef19664720e6ea617b0cd3f4d5
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-20 23:06:18 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -567,3 +567,19 @@ print("Second highest number is : ",\
 	str(secondmax))
 
 #45
+# Python program to find largest number
+# in a list
+
+# List of numbers
+list1 = [10, 20, 20, 4, 45, 45, 45, 99, 99]
+
+# Removing duplicates from the list
+list2 = list(set(list1))
+
+# Sorting the list
+list2.sort()
+
+# Printing the second last element
+print("Second largest element is:", list2[-2])
+
+#46
