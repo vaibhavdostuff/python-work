b08f152967ce4c6c4cec74c7c7aa1f2d859585ac
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-21 20:30:34 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -619,3 +619,22 @@ print("Second largest element is:", list1[-2])
 print("Second largest element is:", sorted(list1)[-2])
 
 #48
+def findLargest(arr):
+	secondLargest = 0
+	largest = min(arr)
+
+	for i in range(len(arr)):
+		if arr[i] > largest:
+			secondLargest = largest
+			largest = arr[i]
+		else:
+			secondLargest = max(secondLargest, arr[i])
+
+	# Returning second largest element
+	return secondLargest
+
+
+# Calling above method over this array set
+print(findLargest([10, 20, 4, 45, 99]))
+
+#49
