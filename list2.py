47e1ea4c30a66edd2548c1fc2b054cbeace9faa7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-22 23:55:30 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -783,3 +783,20 @@ for num in list1:
 		print(num, end=" ")
 
 #59
+# Python program to print Even Numbers in a List
+
+# Initializing list and value
+list1 = [10, 24, 4, 45, 66, 93]
+num = 0
+
+# Uing while loop
+while(num < len(list1)):
+
+	# Cecking condition
+	if list1[num] % 2 == 0:
+		print(list1[num], end=" ")
+
+	# increment num
+	num += 1
+
+#60
