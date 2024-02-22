da9bc41ccfcc553b035053fd4544e44a70157640
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-22 23:53:41 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -731,3 +731,13 @@ N = 2
 Nmaxelements(list1, N)
 
 #55
+# Python program to find N largest
+# element from given list of integers
+
+l = [1000, 298, 3579, 100, 200, -45, 900]
+n = 4
+
+l.sort()
+print(l[-n:])
+
+#56
