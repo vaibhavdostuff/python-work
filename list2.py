6842b56f70472764f86c32c3fd58fa70fe118cd0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-22 23:54:02 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -741,3 +741,21 @@ l.sort()
 print(l[-n:])
 
 #56
+import heapq
+
+
+def find_n_largest_elements(lst, N):
+	heap = lst
+	return heapq.nlargest(N, heap)
+
+
+# Test the function with given inputs
+lst = [4, 5, 1, 2, 9]
+N = 2
+print(find_n_largest_elements(lst, N))
+
+lst = [81, 52, 45, 10, 3, 2, 96]
+N = 3
+print(find_n_largest_elements(lst, N))
+
+#57
