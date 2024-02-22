f5372c2311eae66526eaa6def93975d6c51d840d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-22 23:56:39 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -823,3 +823,25 @@ even_nos = list(filter(lambda x: (x % 2 == 0), list1))
 print("Even numbers in the list: ", even_nos)
 
 #62
+# Python program to print
+# even numbers in a list using recursion
+
+
+def evennumbers(list, n=0):
+
+	# base case
+	if n == len(list):
+		exit()
+	if list[n] % 2 == 0:
+		print(list[n], end=" ")
+	
+	# calling function recursively
+	evennumbers(list, n+1)
+
+# Initializing list 
+list1 = [10, 21, 4, 45, 66, 93]
+
+print("Even numbers in the list:", end=" ")
+evennumbers(list1)
+
+#63
