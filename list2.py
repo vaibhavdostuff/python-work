9c943bf2854cf05d50ace1bb17b69be4b8171343
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-22 23:56:15 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -811,3 +811,15 @@ even_nos = [num for num in list1 if num % 2 == 0]
 print("Even numbers in the list: ", even_nos)
 
 #61
+# Python program to print Even Numbers in a List
+
+# list of numbers
+list1 = [10, 21, 4, 45, 66, 93, 11]
+
+
+# we can also print even no's using lambda exp.
+even_nos = list(filter(lambda x: (x % 2 == 0), list1))
+
+print("Even numbers in the list: ", even_nos)
+
+#62
