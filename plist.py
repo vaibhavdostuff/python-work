4ea18eeb0c3fd5b1dea888842ecf02da87bfbf97
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-23 21:38:56 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -14,4 +14,17 @@ for i in list1:
 
 
 #3
+# Python code To print all even numbers
+# in a given list using numpy array
+import numpy as np
+
+# Declaring Range
+temp = [2, 7, 5, 64, 14]
+li = np.array(temp)
+
+# printing even numbers using numpy array
+even_num = li[li % 2 == 0]
+print(even_num)
+
+#4
 		
