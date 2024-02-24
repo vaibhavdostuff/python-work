c5675111ec581cde216c60021e1efa7823ffda7f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-24 19:54:00 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -201,3 +201,17 @@ for num in range(start, end + 1):
 		print(num, end=" ")
 
 #18
+# Python program to print Even Numbers in given range
+
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
+
+#creating even starting range
+start = start+1 if start&1 else start
+
+
+#create a list and printing element
+#contains Even numbers in range
+[ print( x ) for x in range(start, end + 1, 2)]
+
+#19
