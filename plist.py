665e46fda2cc35be4a305760b0f7479bed92ad1a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-25 23:42:46 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -278,3 +278,16 @@ for num in range(start, end + 1):
 		print(num, end = " ")
 
 #26
+# Python program to print Even Numbers in given range
+
+start = int(input("Enter the start of range:"))
+end = int(input("Enter the end of range:"))
+
+# iterating each number in list
+for num in range(start, end + 1):
+
+	# checking condition
+	if num % 2 != 0:
+		print(num)
+
+#27
