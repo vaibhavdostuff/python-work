f6ceeed5b13a0835e211a0f5c30c2c3942176849
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-25 23:42:22 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -266,3 +266,15 @@ even_num = li[li%2==0];
 print(even_num)
 
 #25
+# Python program to print odd Numbers in given range
+
+start, end = 4, 19
+
+# iterating each number in list
+for num in range(start, end + 1):
+	
+	# checking condition
+	if num % 2 != 0:
+		print(num, end = " ")
+
+#26
