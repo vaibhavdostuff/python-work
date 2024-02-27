269893e9563eaeef7692a73f582120772d66e8b8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-27 22:39:14 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -418,3 +418,19 @@ list1 = [-10, -21, -4, 45, -66, 93]
 pos_nos = [num for num in list1 if num >= 0]
 
 print("Positive numbers in the list: ", *pos_nos)
+
+#38
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [11, -21, 0, 45, 66, -93]
+
+# iterating each number in list
+for num in list1:
+
+	# checking condition
+	if num & gt = 0:
+		print(num, end= & quot & quot)
+
+#39
+		
