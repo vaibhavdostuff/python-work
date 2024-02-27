a5a592fa8e9ce94cc9bbcec56cae4bea116c4af3
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-27 22:39:38 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -433,4 +433,21 @@ for num in list1:
 		print(num, end= & quot & quot)
 
 #39
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [-10, 21, -4, -45, -66, 93]
+num = 0
+
+# using while loop	 
+while(num < len(list1)):
+	
+	# checking condition
+	if list1[num] >= 0:
+		print(list1[num], end = " ")
+	
+	# increment num 
+	num += 1
+	
+#40
 		
