3cf18e4f6c584769bc6dac4bd826af415e88174c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-27 22:40:03 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -450,4 +450,16 @@ while(num < len(list1)):
 	num += 1
 	
 #40
+# Python program to print positive Numbers in a List
+
+# list of numbers 
+list1 = [-10, 21, 4, -45, -66, 93, -11] 
+
+
+# we can also print positive no's using lambda exp. 
+pos_nos = list(filter(lambda x: (x >= 0), list1))
+
+print("Positive numbers in the list: ", *pos_nos) 
+
+#41
 		
