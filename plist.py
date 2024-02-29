0b0d1b7d323ef46defd10fa2c118e19f745d2b16
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-29 18:31:52 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -544,4 +544,16 @@ neg_nos = [num for num in list1 if num < 0]
 print("Negative numbers in the list: ", *neg_nos)
 
 #48
+# Python program to print negative Numbers in a List
+
+# list of numbers
+list1 = [-10, 21, 4, -45, -66, 93, -11]
+
+
+# we can also print negative no's using lambda exp.
+neg_nos = list(filter(lambda x: (x < 0), list1))
+
+print("Negative numbers in the list: ", *neg_nos)
+
+#49
 		
