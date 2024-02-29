6c11a75ff3c9ec147ddee788ed76e66f8b3961ca
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-29 18:31:30 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -533,4 +533,15 @@ while(num < len(list1)):
 	num += 1
 
 #47
+# Python program to print negative Numbers in a List
+
+# list of numbers
+list1 = [-10, -21, -4, 45, -66, 93]
+
+# using list comprehension
+neg_nos = [num for num in list1 if num < 0]
+
+print("Negative numbers in the list: ", *neg_nos)
+
+#48
 		
