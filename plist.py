59d0128ae0b9a785548e1fa0a5fd442d97bd1ed2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-03 23:40:55 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -820,3 +820,16 @@ for ele in list1:
 print("New list after removing all even numbers: ", list1)
 
 #73
+# Python program to remove multiple
+# elements from a list 
+
+# creating a list
+list1 = [11, 5, 17, 18, 23, 50] 
+
+# will create a new list, 
+# excluding all even numbers
+list1 = [ elem for elem in list1 if elem % 2 != 0]
+
+print(*list1)
+
+#74
