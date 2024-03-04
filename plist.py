5c45c0dc28bc9ac327586116084fa25b30cc4d66
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-04 22:01:02 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -959,3 +959,23 @@ print(f"The original list is : {input_list}")
 print(f"List after empty list removal : {empty_list_remove(input_list)}")
 
 #82
+# Python3 code to Demonstrate Remove empty List
+
+# Initializing list
+test_list = [5, 6, [], 3, [], [], 9]
+
+# printing original list
+print("The original list is : " + str(test_list))
+new_list=[]
+# Remove empty List from List
+for i in test_list:
+	x=str(type(i))
+	if(x.find('list')!=-1):
+		if(len(i)!=0):
+			new_list.append(i)
+	else:
+		new_list.append(i)
+# printing result
+print("List after empty list removal : " + str(new_list))
+
+#83
