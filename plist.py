d8dc6615bb4b4d80ed8863e08322e67b2fd3a9aa
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-04 22:01:52 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -995,3 +995,19 @@ while [] in test_list :
 print("List after empty list removal : " + str(test_list))
 
 #84
+# Python3 code to Demonstrate Remove empty List
+
+# Initializing list
+test_list = [5, 6, [], 3, [], [], 9]
+
+# printing original list
+print("The original list is : " + str(test_list))
+x=list(map(str,test_list))
+y="".join(x)
+y=y.replace("[]","")
+y=list(map(int,y))
+
+# printing result
+print("List after empty list removal : " + str(y))
+
+#85
