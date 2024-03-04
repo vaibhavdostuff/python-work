a0caa4a8dd53a48838af34ff9d25f758e1021315
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-04 22:02:29 +0530

    list python programm

--- plist.py
+++ plist.py
@@ -1016,3 +1016,18 @@ res = [ele for i,ele in enumerate(test_list) if ele != []]
 print(res)
 
 #86
+# Python3 code to Demonstrate Remove empty List
+
+# Initializing list
+test_list = [5, 6, [], 3, [], [], 9]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Remove empty List from List
+res = filter(None, test_list)
+
+# printing result
+print("List after empty list removal : " ,res)
+
+#87
