18ff9db52686aa3be4571cf8405d2ad2ce4ef9ba
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-05 22:15:36 +0530

    list python programm

--- list.py
+++ list.py
@@ -514,3 +514,36 @@ print("List before clearing: ",lst)
 # Clearing list using slicing
 lst = lst[:0]
 print("List after clearing using Slicing: ",lst)
+
+#37
+# Python program to copy or clone a list
+# Using the Slice Operator
+def Cloning(li1):
+	li_copy = li1[:]
+	return li_copy
+
+
+# Driver Code
+li1 = [4, 8, 2, 10, 15, 18]
+li2 = Cloning(li1)
+print("Original List:", li1)
+print("After Cloning:", li2)
+
+#38
+# Python code to clone or copy a list
+# Using the in-built function extend()
+
+
+def Cloning(li1):
+	li_copy = []
+	li_copy.extend(li1)
+	return li_copy
+
+
+# Driver Code
+li1 = [4, 8, 2, 10, 15, 18]
+li2 = Cloning(li1)
+print("Original List:", li1)
+print("After Cloning:", li2)
+
+#39
