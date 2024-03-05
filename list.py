27602debaa9e0d66845d583b50d8cbf21d5e314c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-05 22:15:56 +0530

    list python programm

--- list.py
+++ list.py
@@ -547,3 +547,16 @@ print("Original List:", li1)
 print("After Cloning:", li2)
 
 #39
+# Python code to clone or copy a list 
+# Using the List copy using =
+def Cloning(li1): 
+	li_copy = li1
+	return li_copy 
+
+# Driver Code 
+li1 = [4, 8, 2, 10, 15, 18] 
+li2 = Cloning(li1) 
+print("Original List:", li1) 
+print("After Cloning:", li2)
+
+#40
