7e9e5cec128f5f6fe90d82f840a9216e7ff97052
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-06 19:50:12 +0530

    list python programm

--- list.py
+++ list.py
@@ -640,3 +640,15 @@ print("Original List:", lst)
 print("After Cloning:", *li_copy)
 
 #47
+from collections import deque
+
+original_list = [4, 8, 2, 10, 15, 18]
+
+copied_list = deque(original_list)
+copied_list = list(copied_list)
+
+print("Original List:", original_list)
+
+print("After Cloning:", copied_list)
+
+#48
