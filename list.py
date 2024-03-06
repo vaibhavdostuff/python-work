55ad9011d2bb2c2dcf277ab856a12144bef5c1fd
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-06 19:52:08 +0530

    list python programm

--- list.py
+++ list.py
@@ -673,3 +673,9 @@ x=[i for i in l if i==ele]
 print("the element",ele,"occurs",len(x),"times")
 
 #50
+l = [1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5] 
+ele=1
+x=[i for j,i in enumerate(l) if i==ele] 
+print("the element",ele,"occurs",len(x),"times")
+
+#51
