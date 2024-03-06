dc862b5a93b8ab1555775e36f2881d5a780a4931
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-06 19:49:27 +0530

    list python programm

--- list.py
+++ list.py
@@ -624,3 +624,11 @@ li3 = copy.deepcopy(li1)
 print(li3)
 
 #45
+# Python code to clone or copy a list 
+# Using list comprehension 
+lst = [4, 8, 2, 10, 15, 18] 
+li_copy = [i for a,i in enumerate(lst)] 
+print("Original List:", lst) 
+print("After Cloning:", li_copy) 
+
+#46
