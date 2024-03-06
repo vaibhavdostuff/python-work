4306799df3016362d59d116cae8ef630e7d810ee
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-06 19:51:19 +0530

    list python programm

--- list.py
+++ list.py
@@ -652,3 +652,18 @@ print("Original List:", original_list)
 print("After Cloning:", copied_list)
 
 #48
+# Python code to count the number of occurrences
+def countX(lst, x):
+	count = 0
+	for ele in lst:
+		if (ele == x):
+			count = count + 1
+	return count
+
+
+# Driver Code
+lst = [8, 6, 8, 10, 8, 20, 10, 8, 8]
+x = 8
+print('{} has occurred {} times'.format(x, countX(lst, x)))
+
+#49
