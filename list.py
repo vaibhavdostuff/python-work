ad65ad72de424f11686ee4a2e8e234bebf17e9cc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-07 11:25:30 +0530

    list python programm

--- list.py
+++ list.py
@@ -727,3 +727,16 @@ print("Element Count")
 print(count)
 
 #56
+# Python program to remove empty tuples from a 
+# list of tuples function to remove empty tuples 
+# using list comprehension
+def Remove(tuples):
+	tuples = [t for t in tuples if t]
+	return tuples
+
+# Driver Code
+tuples = [(), ('ram','15','8'), (), ('laxman', 'sita'), 
+		('krishna', 'akbar', '45'), ('',''),()]
+print(Remove(tuples))
+
+#57
