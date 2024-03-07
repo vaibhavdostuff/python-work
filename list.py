2e93bd196ec9d9098f8ea63cc7215df82ee9c012
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-07 11:29:10 +0530

    list python programm

--- list.py
+++ list.py
@@ -767,3 +767,21 @@ tuples = [(), ('ram', '15', '8'), (), ('laxman', 'sita'),
 print(Remove(tuples))
 
 #59
+# Python program to remove empty tuples from a
+# list of tuples function to remove empty tuples
+# using len()
+
+
+def Remove(tuples):
+	for i in tuples:
+		if(len(i) == 0):
+			tuples.remove(i)
+	return tuples
+
+
+# Driver Code
+tuples = [(), ('ram', '15', '8'), (), ('laxman', 'sita'),
+		('krishna', 'akbar', '45'), ('', ''), ()]
+print(Remove(tuples))
+
+#60
