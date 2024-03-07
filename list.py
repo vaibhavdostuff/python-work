bfe8d20cd4a7be5c5152e859ce044fc1dce600c3
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-07 11:23:18 +0530

    list python programm

--- list.py
+++ list.py
@@ -691,3 +691,14 @@ print('{} has occurred {} times'.format(x,
 										countX(lst, x)))
 
 #52
+from collections import Counter
+
+# declaring the list
+l = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+# driver program
+x = 3
+d = Counter(l)
+print('{} has occurred {} times'.format(x, d[x]))
+
+#53
