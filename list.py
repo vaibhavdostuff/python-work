7ce735a4be563618e54201a4ac6e8e0a555cd9ed
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-08 23:54:51 +0530

    list python programm

--- list.py
+++ list.py
@@ -860,3 +860,19 @@ tuples = [(), ('ram','15','8'), (), ('laxman', 'sita'),
 print(Remove(tuples)) # call the Remove function and print the output
 
 #66
+# Python program to print duplicates from 
+# a list of integers
+lis = [1, 2, 1, 2, 3, 4, 5, 1, 1, 2, 5, 6, 7, 8, 9, 9]
+
+uniqueList = []
+duplicateList = []
+
+for i in lis:
+	if i not in uniqueList:
+		uniqueList.append(i)
+	elif i not in duplicateList:
+		duplicateList.append(i)
+
+print(duplicateList)
+
+#67
