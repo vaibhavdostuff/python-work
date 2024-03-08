ad8db754f1a94c96ca1fc4b9035137a8d9c880bf
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-08 23:55:08 +0530

    list python programm

--- list.py
+++ list.py
@@ -876,3 +876,13 @@ for i in lis:
 print(duplicateList)
 
 #67
+from collections import Counter
+
+l1 = [1,2,1,2,3,4,5,1,1,2,5,6,7,8,9,9]
+d = Counter(l1)
+print(d)
+
+new_list = list([item for item in d if d[item]>1])
+print(new_list)
+
+#68
