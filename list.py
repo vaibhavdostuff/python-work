2558d9b7604ae4353b2ddc86b8f640a67cc76e3e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-09 19:13:50 +0530

    list python programm

--- list.py
+++ list.py
@@ -1011,3 +1011,22 @@ res = list(map(lambda ele: sum(int(sub) for sub in str(ele)), test_list))
 print ("List Integer Summation : " + str(res))
 
 #77
+# Python3 code to demonstrate
+# Sum of number digits in a List
+# using sum() + reduce()
+from functools import reduce
+
+# Initializing list
+test_list = [12, 67, 98, 34]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# Sum of number digits in List
+# using sum() + reduce()
+res = [reduce(lambda x, y: int(x) + int(y), list(str(i))) for i in test_list]
+
+# printing result
+print("List Integer Summation : " + str(res))
+
+#78
