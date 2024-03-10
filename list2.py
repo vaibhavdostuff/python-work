10fc4cac6ae8eeefaa3089d210a9139f04eb4379
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-10 12:04:18 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -845,3 +845,23 @@ print("Even numbers in the list:", end=" ")
 evennumbers(list1)
 
 #63
+my_list = ['vaibhav', 'is', 'here', 'like', 
+		'fammily','nerdy', 'negi', 'love', 
+			'questions','words', 'life'] 
+
+# Yield successive n-sized 
+# chunks from l. 
+def divide_chunks(l, n): 
+	
+	# looping till length l 
+	for i in range(0, len(l), n): 
+		yield l[i:i + n] 
+
+# How many elements each 
+# list should have 
+n = 5
+
+x = list(divide_chunks(my_list, n)) 
+print (x) 
+
+#64
