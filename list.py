952e2122206164019f96bf7af60c823c1d70af67
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-10 12:01:54 +0530

    list python programm

--- list.py
+++ list.py
@@ -1046,3 +1046,17 @@ res = np.sum([list(map(int, str(ele))) for ele in test_list], axis=1)
 print("List Integer Summation : " + str(list(res)))
 
 #79
+lst = [12, 67, 98, 34]
+def digit_sum(num):
+	digit_sum = 0
+	while num > 0:
+		digit_sum += num % 10
+		num //= 10
+	return digit_sum
+
+def sum_of_digits_list(lst):
+	return list(map(digit_sum, lst))
+
+print(sum_of_digits_list(lst))
+
+#80
