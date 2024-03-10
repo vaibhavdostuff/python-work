afed6bd481d67447fcb968f6093b09a99fd6804b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-10 12:05:19 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -875,3 +875,15 @@ for i in range(start, end, step):
 	print(my_list[x:x+step]) 
 
 #65
+my_list = [1, 2, 3, 4, 5, 
+			6, 7, 8, 9] 
+
+# How many elements each 
+# list should have 
+n = 4
+
+# using list comprehension 
+final = [my_list[i * n:(i + 1) * n] for i in range((len(my_list) + n - 1) // n )] 
+print (final) 
+
+#66
