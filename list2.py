0a3b08abefcb1c6d3457149187e8ba2db135169b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-10 12:05:00 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -865,3 +865,13 @@ x = list(divide_chunks(my_list, n))
 print (x) 
 
 #64
+my_list = [1, 2, 3, 4, 5, 
+		6, 7, 8, 9] 
+start = 0
+end = len(my_list) 
+step = 3
+for i in range(start, end, step): 
+	x = i 
+	print(my_list[x:x+step]) 
+
+#65
