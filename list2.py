4b4b4431dbf5d38a6532cfd94959da65d18b67db
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-11 22:33:30 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -970,3 +970,17 @@ y = [0, 1, 1, 0, 1, 2, 2, 0, 1]
 print(sort_list(x, y))
 
 #72
+def sorting_of_element(list1, list2):
+
+	# initializing blank dictionary
+	f_1 = {}
+
+	# initializing blank list
+	final_list = []
+
+	# Addition of two list in one dictionary
+	f_1 = {list1[i]: list2[i] for i in range(len(list2))}
+
+	# sorting of dictionary based on value
+	f_lst = {k: v for k, v in sorted(f_1.items(), key=lambda item: item[1])}
+
