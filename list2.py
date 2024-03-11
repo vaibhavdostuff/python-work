537d5e95924c799ef5363b915f2dc9d69e60b883
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-11 22:34:05 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -998,3 +998,16 @@ list3 = sorting_of_element(list1, list2)
 print(list3)
 
 #73
+# Python program to sort values of first list based on second list
+list1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+list2 = [0, 1, 1, 0, 1, 2, 2, 0, 1]
+a = list(set(list2))
+a.sort()
+res = []
+for i in a:
+	for j in range(0, len(list2)):
+		if(list2[j] == i):
+			res.append(list1[j])
+print(res)
+
+#74
