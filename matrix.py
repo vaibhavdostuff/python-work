8fd93e612306155a11a78022656bb8623c9a7170
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-12 09:26:19 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -76,4 +76,25 @@ result = np.array(X) + np.array(Y)
 print(result)
 
 #5
+from sympy import Matrix
+
+X = [[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]]
+
+Y = [[9, 8, 7],
+	[6, 5, 4],
+	[3, 2, 1]]
+
+# Create Matrix objects from the lists
+matrix_x = Matrix(X)
+matrix_y = Matrix(Y)
+
+# Add the matrices
+result = matrix_x + matrix_y
+
+# Print the result
+print(result)
+
+#6
 	
