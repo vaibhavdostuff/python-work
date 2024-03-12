3eec2740e07441e858c3ef47dddb11d932e85fae
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-12 09:22:12 +0530

    list python programm

--- list2.py
+++ list2.py
@@ -1020,3 +1020,28 @@ res = list(OrderedDict(sorted(d.items(), key=lambda x: x[1])))
 print(res)
 
 #75
+import numpy as np
+
+# Define a function that takes two lists as input, sorts the first list based on
+# the order of the second list, and returns the sorted list as a numpy array
+def sort_list(list1, list2):
+	# Use numpy's argsort function to get the indices that would sort the second list
+	idx = np.argsort(list2)
+	# Index the first list using the sorted indices and return the result as a numpy array
+	return np.array(list1)[idx]
+
+# Define two lists of strings and integers to be used as inputs for the sort_list function
+x = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+y = [0, 1, 1, 0, 1, 2, 2, 0, 1]
+
+# Call the sort_list function with the two lists and print the result
+print(sort_list(x, y))
+
+# Define another two lists of strings and integers to be used as inputs for the sort_list function
+x = ["g", "e", "e", "k", "s", "f", "o", "r", "g", "e", "e", "k", "s"]
+y = [0, 1, 1, 0, 1, 2, 2, 0, 1]
+
+# Call the sort_list function with the two lists and print the result
+print(sort_list(x, y))
+
+#76
