b282d40168553620b8d9ee60bedca67cb73386e2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-12 09:25:53 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -58,4 +58,22 @@ result = [map(sum, zip(*t)) for t in zip(X, Y)]
 print(result)
 
 #4
+# Program to add two matrices
+# using numpy
+
+import numpy as np
+
+X = [[1,2,3],
+	[4 ,5,6],
+	[7 ,8,9]]
+
+Y = [[9,8,7],
+	[6,5,4],
+	[3,2,1]]
+
+result = np.array(X) + np.array(Y)
+
+print(result)
+
+#5
 	
