f19490e35aa11f798ab58a34c75367074f3c35a0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-13 18:12:48 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -234,3 +234,24 @@ res = prod(list(chain(*test_list)))
 print("The total element product in lists is : " + str(res))
 
 #11
+# Python3 code to demonstrate
+# Matrix Product
+
+# initializing list
+test_list = [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
+
+# printing original list
+print("The original list : " + str(test_list))
+
+x = []
+for i in test_list:
+	x.extend(i)
+res = 1
+for j in x:
+	res *= j
+
+
+# print result
+print("The total element product in lists is : " + str(res))
+
+#12
