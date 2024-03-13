5ebe286bdbb8f1c77b2b8169b05460257916599e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-13 18:14:02 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -316,3 +316,23 @@ res = multiply_nested_list(test_list)
 print("The total element product in lists is : " + str(res))
 
 #15
+# importing numpy as np
+import numpy as np
+
+
+# creating first matrix
+A = np.array([[1, 2], [3, 4]])
+
+# creating second matrix
+B = np.array([[4, 5], [6, 7]])
+
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
+
+# adding two matrix
+print("Addition of two matrix")
+print(np.add(A, B))
+
+#16
