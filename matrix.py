49e8d94adc6a0cc317373ad345a705b0f5b04e30
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-13 18:13:18 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -276,3 +276,20 @@ res = reduce(operator.mul, x, 1)
 print("The total element product in lists is : " + str(res))
 
 #13
+# Python3 code to demonstrate
+# Matrix Product
+
+# initializing list
+test_list = [[1, 4, 5], [7, 3], [4], [46, 7, 3]]
+
+# printing original list
+print("The original list : " + str(test_list))
+res = 1
+for i in test_list:
+	for j in i:
+		res *= j
+
+# print result
+print("The total element product in lists is : " + str(res))
+
+#14
