c2300475fef5356fbf5d330c051d7c4e298ae148
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-15 23:55:43 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -456,4 +456,24 @@ res = [list(range(1 + N * i, 1 + N * (i + 1)))
 print("The created matrix of N * N: " + str(res))
 
 #24
+# Python3 code to demonstrate
+# matrix creation of n * n
+# using next() + itertools.count()
+import itertools
+
+# initializing N
+N = 4
+
+# printing dimension
+print("The dimension : " + str(N))
+
+# using next() + itertools.count()
+# matrix creation of n * n
+temp = itertools.count(1) 
+res = [[next(temp) for i in range(N)] for i in range(N)]
+
+# print result
+print("The created matrix of N * N: " + str(res))
+
+#25
 	
