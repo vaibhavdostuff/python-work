93327ddcd56e7b5b02c444d7d8ddcbe44d2298ea
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-15 23:56:53 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -522,4 +522,25 @@ res = [sub[K] for sub in test_list]
 print("The Kth column of matrix is : " + str(res))
 
 #27
+# Python code to demonstrate working of
+# Get Kth Column of Matrix
+# using zip()
+
+# initialize list
+test_list = [[4, 5, 6], [8, 1, 10], [7, 12, 5]]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# initialize K
+K = 2
+
+# Get Kth Column of Matrix
+# using zip()
+res = list(zip(*test_list)[K])
+
+# printing result
+print("The Kth column of matrix is : " + str(res))
+
+#28
 	
