915932c63a64ef93ab891b6b2864a3a2f7a4046f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-15 23:55:21 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -437,4 +437,23 @@ print(transpose2(matrix))
 print("Time taken", end-start, "ns")
 
 #23
+# Python3 code to demonstrate
+# matrix creation of n * n
+# using list comprehension
+
+# initializing N
+N = 4
+
+# printing dimension
+print("The dimension : " + str(N))
+
+# using list comprehension
+# matrix creation of n * n
+res = [list(range(1 + N * i, 1 + N * (i + 1)))
+							for i in range(N)]
+
+# print result
+print("The created matrix of N * N: " + str(res))
+
+#24
 	
