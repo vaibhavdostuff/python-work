bb19ae67c239fa7ac78aaebed8c86359749606f5
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-15 23:56:03 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -476,4 +476,29 @@ res = [[next(temp) for i in range(N)] for i in range(N)]
 print("The created matrix of N * N: " + str(res))
 
 #25
+# Python3 code to demonstrate
+# matrix creation of n * n
+
+# initializing N
+N = 4
+
+# printing dimension
+print("The dimension : " + str(N))
+
+# using list comprehension
+# matrix creation of n * n
+x=N*N
+y=[]
+for i in range(1,x+1):
+	y.append(i)
+res=[]
+i=0
+while(i<len(y)):
+	a=y[i:i+N]
+	res.append(a)
+	i+=N
+# print result
+print("The created matrix of N * N: " + str(res))
+
+#26
 	
