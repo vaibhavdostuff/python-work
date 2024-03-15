f19b030f9e372d985a65806c82b68160eb3fa25d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-15 23:53:41 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -388,3 +388,12 @@ for row in result:
 	print()
 
 #18
+m = [[1, 2], [3, 4], [5, 6]]
+for row in m:
+	print(row)
+rez = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
+print("\n")
+for row in rez:
+	print(row)
+
+#19
