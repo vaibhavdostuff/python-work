5e5707e32951b9b33908bd3c603eea4946c351e2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-16 17:01:28 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -621,3 +621,22 @@ res = [ele for ele in res if ele]
 print("List after column Concatenation : " + str(res))
 
 #32	
+# Python3 code to demonstrate working of 
+# Vertical Concatenation in Matrix
+# Using join() + list comprehension + zip_longest()
+from itertools import zip_longest
+
+# initializing lists
+test_list = [["Gfg", "good"], ["is", "for"], ["Best"]]
+
+# printing original list
+print("The original list : " + str(test_list))
+
+# using join to concaternate, zip_longest filling values using 
+# "fill"
+res = ["".join(ele) for ele in zip_longest(*test_list, fillvalue ="")]
+
+# printing result 
+print("List after column Concatenation : " + str(res))
+
+#33
