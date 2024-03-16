1564fe7ccf3056b714a3b054142dad761e6cdb91
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-16 17:05:33 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -8,3 +8,17 @@ datetime_INDIA = datetime.now(tz_INDIA)
 print("INDIA time:", datetime_INDIA.strftime("%H:%M:%S"))
 
 #2
+from datetime import datetime
+
+# now() method is used to
+# get object containing
+# current date & time.
+now_method = datetime.now()
+
+# strftime() method used to
+# create a string representing
+# the current time.
+currentTime = now_method.strftime("%H:%M:%S")
+print("Current Time =", currentTime)
+
+#3
