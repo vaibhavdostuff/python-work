1f8e7a62514f3e45f663001a24a160b29e88e1da
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-16 17:00:27 +0530

    matrix python programm

--- matrix.py
+++ matrix.py
@@ -571,4 +571,25 @@ res = list(map(lambda x: x[K], test_list))
 print("The Kth column of matrix is : " + str(res))
 
 #30
+# Python code to demonstrate working of
+# Get Kth Column of Matrix
+# using list slicing
+
+# initialize list
+test_list = [[4, 5, 6], [8, 1, 10], [7, 12, 5]]
+
+# printing original list
+print("The original list is : " + str(test_list))
+
+# initialize K
+K = 2
+
+# Get Kth Column of Matrix
+# using list slicing
+res = [row[K] for row in test_list]
+
+# printing result
+print("The Kth column of matrix is : " + str(res))
+
+#31
 	
