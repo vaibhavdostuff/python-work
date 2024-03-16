c25158bb4e4f6f063c7bccdbf9086370ca97891b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-16 17:06:10 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -31,3 +31,18 @@ time = datetime.now().time()
 print("Current Time =", time)
 
 #4
+import time
+
+
+# localtime() method used to
+# get the object containing
+# the local time.
+Time = time.localtime()
+
+# strftime() method used to
+# create a string representing
+# the current time.
+currentTime = time.strftime("%H:%M:%S", Time)
+print(currentTime)
+
+#5
