2133da6bca8cb447112e64f14b920ee71a1d5d04
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-17 18:31:36 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -173,3 +173,15 @@ print("Today = ", presentday.strftime('%d-%m-%Y'))
 print("Tomorrow = ", tomorrow.strftime('%d-%m-%Y'))
 
 #17
+import calendar
+from datetime import date, timedelta
+
+today = date.today()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+
+print("Yesterday = ", calendar.day_name[yesterday.weekday()], yesterday.strftime('%d-%m-%Y'))
+print("Today = ", calendar.day_name[today.weekday()], today.strftime('%d-%m-%Y'))
+print("Tomorrow = ", calendar.day_name[tomorrow.weekday()], tomorrow.strftime('%d-%m-%Y'))
+
+#18
