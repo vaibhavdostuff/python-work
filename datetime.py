8906ed2486392e72b6a2d7bd6997b41b0e09e330
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-17 18:28:06 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -85,3 +85,16 @@ print("Second :", current_time.second)
 print("Microsecond :", current_time.microsecond)
 
 #8
+# for now()
+import datetime
+
+# for timezone()
+import pytz
+
+# using now() to get current time
+current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+
+# printing current time in india
+print("The current time in india is :", current_time)
+
+#9
