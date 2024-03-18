59af329e82b983fdb8dab5702e063b707f4b7269
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-18 23:56:31 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -326,3 +326,20 @@ timestamp = time.mktime(tuple)
 print(timestamp)
 
 #25
+# Python program to convert
+# date to timestamp
+
+
+import time
+import datetime
+
+
+string = "20/01/2020"
+
+
+element = datetime.datetime.strptime(string,"%d/%m/%Y")
+
+timestamp = datetime.datetime.timestamp(element)
+print(timestamp)
+
+#26
