a6aa52bbb701d14ae932e48b0e6ae5c59591f08e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-18 23:54:05 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -238,3 +238,17 @@ print(convert24('11:21:30 PM')) # Output: '23:21:30'
 print(convert24('12:12:20 AM')) # Output: '00:12:20'
 
 #21
+import re
+
+def convert_to_24hour(time_str):
+	hour, minute, second, am_pm = re.findall('\d+|\w+', time_str)
+	hour = int(hour)
+	if am_pm == 'PM' and hour != 12:
+		hour += 12
+	elif am_pm == 'AM' and hour == 12:
+		hour = 0
+	return f'{hour:02d}:{minute}:{second}'
+print(convert_to_24hour('11:21:30 PM')) 
+print(convert_to_24hour('12:12:20 AM'))
+
+#22
