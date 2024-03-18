62e8e48c6f21f723c2bea8b8e48d97c4e1dfbce2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-18 23:53:50 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -214,3 +214,27 @@ def convert24(str1):
 print(convert24("08:05:45 PM")) 
 
 #19
+from datetime import datetime
+
+def convert24(time):
+	# Parse the time string into a datetime object
+	t = datetime.strptime(time, '%I:%M:%S %p')
+	# Format the datetime object into a 24-hour time string
+	return t.strftime('%H:%M:%S')
+
+print(convert24('11:21:30 PM')) # Output: '23:21:30'
+print(convert24('12:12:20 AM')) # Output: '00:12:20'
+
+#20
+from datetime import datetime
+
+def convert24(time):
+	# Parse the time string into a datetime object
+	t = datetime.strptime(time, '%I:%M:%S %p')
+	# Format the datetime object into a 24-hour time string
+	return t.strftime('%H:%M:%S')
+
+print(convert24('11:21:30 PM')) # Output: '23:21:30'
+print(convert24('12:12:20 AM')) # Output: '00:12:20'
+
+#21
