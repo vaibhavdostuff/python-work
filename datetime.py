cd19ba578553365a7bacfc0c71fcdfb8d42da40a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-18 23:55:55 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -294,3 +294,16 @@ except KeyboardInterrupt:
 	print("Done")
 
 #23
+# Python program to convert
+# date to timestamp
+
+
+import time
+import datetime
+
+
+string = "20/01/2020"
+print(time.mktime(datetime.datetime.strptime(string,
+											"%d/%m/%Y").timetuple()))
+
+#24
