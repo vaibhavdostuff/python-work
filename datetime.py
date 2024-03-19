682e0d6ec94291db40de6787dbfdfeba0cdf0a2f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-19 22:24:19 +0530

    date_time python programm

--- datetime.py
+++ datetime.py
@@ -403,3 +403,24 @@ def day_occur_time(year):
 	for i in range(7):
 		if day == days[i]:
 			pos = i
+			
+	# mark the occurrence to be 53 of 1st day
+	# and 2nd day if the year is leap year
+	if calendar.isleap(year):
+		L[pos] += 1
+		L[(pos+1)%7] += 1
+		
+	else:
+		L[pos] += 1
+		
+	
+	# Print the days
+	for i in range(7):
+		print(days[i], L[i])
+	
+
+# Driver Code 
+year = 2019
+day_occur_time(year)
+
+#31
