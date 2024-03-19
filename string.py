53710026b0831d035f37a8a4b38bc3d61e2c0a44
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-19 22:26:05 +0530

    date_time python programm

--- string.py
+++ string.py
@@ -210,3 +210,5 @@ print("Formatting in 3.2f format: ")
 print('The value of Integer1 is %3.2f' % Integer1) 
 print("\nFormatting in 3.4f format: ") 
 print('The value of Integer1 is %3.4f' % Integer1) 
+
+#19
