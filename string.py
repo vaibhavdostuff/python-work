239d829dab746ae471051c50eeffe0c7ea81d400
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-19 22:30:44 +0530

    string  python programm

--- string.py
+++ string.py
@@ -304,4 +304,14 @@ if __name__ == "__main__":
 	find_dup_char(input)
 
 #23
-	
+def find_dup_char(input):
+	x = filter(lambda x: input.count(x) >= 2, input)
+	print(' '.join(set(x)))
+
+
+# Driver Code
+if __name__ == "__main__":
+	input = 'vaibhavishere'
+	find_dup_char(input)
+
+#24
