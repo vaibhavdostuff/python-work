52801525cec74ebabcbc20e864ea1e73f53f80b5
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-20 22:52:03 +0530

    string  python programm

--- string.py
+++ string.py
@@ -358,3 +358,33 @@ if __name__ == "__main__":
 	find_dup_char(input)
 
 #27
+def checkEmpty(input, pattern):
+	
+	# If both are empty
+	if len(input)== 0 and len(pattern)== 0:
+		return 'true'
+
+	# If only pattern is empty
+	if len(pattern)== 0:
+		return 'false'
+
+	while (len(input) != 0):
+
+		# find sub-string in main string
+		index = input.find(pattern)
+
+		# check if sub-string founded or not
+		if (index ==(-1)):
+			return 'false'
+
+		# slice input string in two parts and concatenate
+		input = input[0:index] + input[index + len(pattern):]		 
+	return 'true'
+
+# Driver program
+if __name__ == "__main__":
+	input ='vaibhavishere'
+	pattern ='vaibhav'
+	print (checkEmpty(input, pattern))
+
+#28
