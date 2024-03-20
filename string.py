d84c30641b5cfb7eedb86a14f15f6e48a0d80809
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-20 22:56:20 +0530

    string  python programm

--- string.py
+++ string.py
@@ -438,3 +438,11 @@ result = eval(code)
 print(result) # Output: "b"
 
 #31
+import types
+
+code_string = "a = 6+5"
+my_namespace = types.SimpleNamespace()
+exec(code_string, my_namespace.__dict__)
+print(my_namespace.a) # 11
+
+#32
