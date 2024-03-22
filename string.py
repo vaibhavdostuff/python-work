d4f5c86e6778accc2c5d280761d8b873c9ebe1ba
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-22 18:38:39 +0530

    string  python programm

--- string.py
+++ string.py
@@ -548,4 +548,26 @@ for i in list(p):
 		print(''.join(i))
 
 #38
+def generate_permutations(string):
+	if len(string) == 1:
+		return [string]
 
+	permutations = []
+	for i in range(len(string)):
+		fixed_char = string[i]
+		remaining_chars = string[:i] + string[i+1:]
+		for perm in generate_permutations(remaining_chars):
+			permutations.append(fixed_char + perm)
+
+	return permutations
+
+string ='vaibhav'
+
+permutations_list = generate_permutations(string)
+z=set(permutations_list)
+
+for perm in z:
+	print(perm)
+
+
+#39
