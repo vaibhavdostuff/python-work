50e2789ff57fccb702bde55e8d517139e28fb263
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-22 18:38:11 +0530

    string  python programm

--- string.py
+++ string.py
@@ -531,4 +531,21 @@ if __name__ == "__main__":
 	allPermutations(str)
 
 #37
+from itertools import permutations
+import string
+
+s = "GEEK"
+a = string.ascii_letters
+p = permutations(s)
+
+# Create a dictionary
+d = []
+for i in list(p):
+
+	# Print only if not in dictionary
+	if (i not in d):
+		d.append(i)
+		print(''.join(i))
+
+#38
 
