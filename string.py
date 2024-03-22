5b2073761968e1bab0a7f0ea03d42b5757b322d7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-22 18:36:29 +0530

    string  python programm

--- string.py
+++ string.py
@@ -480,4 +480,19 @@ string = 'My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articl
 print("Urls: ", Find(string))
 
 #34
+# Python code to find the URL from an input string
+
+def Find(string):
+	x=string.split()
+	res=[]
+	for i in x:
+		if i.find("https:")==0 or i.find("http:")==0:
+			res.append(i)
+	return res
+			
+# Driver Code
+string = 'My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles in the portal of https://www.geeksforgeeks.org/'
+print("Urls: ", Find(string))
+
+#35
 
