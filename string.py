492b2d4b0c21fa6e04c8ca636e4fbaf43c8ce086
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-22 18:37:37 +0530

    string  python programm

--- string.py
+++ string.py
@@ -513,4 +513,22 @@ for word in words:
 print("URLs:", urls)
 
 #36
+# Function to find permutations of a given string
+from itertools import permutations
+
+def allPermutations(str):
+	
+	# Get all permutations of string 'ABC'
+	permList = permutations(str)
+
+	# print all permutations
+	for perm in list(permList):
+		print (''.join(perm))
+	
+# Driver program
+if __name__ == "__main__":
+	str = 'ABC'
+	allPermutations(str)
+
+#37
 
