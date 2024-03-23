dccd0edf7d44dbac944b84df3c739dc5fc6530e0
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-23 13:42:08 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -176,3 +176,23 @@ dict = {'a': 100, 'b': 200, 'c': 300}
 print("Sum :", returnSum(dict))
 
 #9
+# Python3 Program to find sum of
+# all items in a Dictionary
+
+# Function to print sum
+
+
+def returnSum(dict):
+
+	sum = 0
+	for i in dict:
+		sum = sum + dict[i]
+
+	return sum
+
+
+# Driver Function
+dict = {'a': 100, 'b': 200, 'c': 300}
+print("Sum :", returnSum(dict))
+
+#10
