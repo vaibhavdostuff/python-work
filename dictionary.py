64c7c236fb34f549225a393ad31d0665ce962ad9
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-23 12:46:49 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -20,3 +20,25 @@ res = list(sorted({ele for val in test_dict.values() for ele in val}))
 print("The unique values list is : " + str(res))
 
 #2
+# Python3 code to demonstrate working of
+# Extract Unique values dictionary values
+# Using chain() + sorted() + values()
+from itertools import chain
+
+# initializing dictionary
+test_dict = {'a': [5, 6, 7, 8],
+			'b': [10, 11, 7, 5],
+			'c': [6, 12, 10, 8],
+			'd': [1, 2, 5]}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# Extract Unique values dictionary values
+# Using chain() + sorted() + values()
+res = list(sorted(set(chain(*test_dict.values()))))
+
+# printing result
+print("The unique values list is : " + str(res))
+
+#3
