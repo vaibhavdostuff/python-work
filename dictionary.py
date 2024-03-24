61c4a0dacf8e43e2daeab0726503c762d2228c1a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-24 18:09:16 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -211,3 +211,12 @@ dict = {'a': 100, 'b': 200, 'c': 300}
 print("Sum :", returnSum(dict))
 
 #11
+import functools
+
+dic = {'a': 100, 'b': 200, 'c': 300}
+
+sum_dic = functools.reduce(lambda ac,k: ac+dic[k], dic, 0)
+
+print("Sum :", sum_dic)
+
+#12
