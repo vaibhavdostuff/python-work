b6fbac260e65cf05800ff74426dedd8a93c485ad
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-24 18:10:13 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -248,3 +248,29 @@ print("The dictionary after remove is : ", test_dict)
 del test_dict['Mani']
 
 #14
+# Initializing dictionary
+test_dict = {"Arushi": 22, "Anuradha": 21, "Mani": 21, "Haritha": 21}
+
+# Printing dictionary before removal
+print("The dictionary before performing remove is : " + str(test_dict))
+
+# Using pop() to remove a dict. pair
+# removes Mani
+removed_value = test_dict.pop('Mani')
+
+# Printing dictionary after removal
+print("The dictionary after remove is : " + str(test_dict))
+print("The removed key's value is : " + str(removed_value))
+
+print('\r')
+
+# Using pop() to remove a dict. pair
+# doesn't raise exception
+# assigns 'No Key found' to removed_value
+removed_value = test_dict.pop('Manjeet', 'No Key found')
+
+# Printing dictionary after removal
+print("The dictionary after remove is : " + str(test_dict))
+print("The removed key's value is : " + str(removed_value))
+
+#15
