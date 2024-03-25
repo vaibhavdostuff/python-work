71b56ca01642da50eb122c11ce9467e36a5f1f46
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-25 19:00:46 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -445,3 +445,15 @@ merged_dict['e'] = 6 # updates dict1
 print(merged_dict['e']) # prints 6
 
 #25
+def merge_dictionaries(dict1, dict2):
+	merged_dict = dict1.copy()
+	merged_dict.update(dict2)
+	return merged_dict
+
+# Driver code
+dict1 = {'x': 10, 'y': 8}
+dict2 = {'a': 6, 'b': 4}
+
+print(merge_dictionaries(dict1, dict2))
+
+#26
