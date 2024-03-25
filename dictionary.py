f4c2e8b1b905a1aaa854fd660f1fdf91b44103f7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-25 19:13:54 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -495,3 +495,23 @@ res = dict(zip(test_dict['month'], test_dict['name']))
 print("Flattened dictionary : " + str(res)) 
 
 #28
+# Python3 code to demonstrate working of
+# Convert key-values list to flat dictionary
+# initializing dictionary
+test_dict = {'month' : [1, 2, 3],
+			'name' : ['Jan', 'Feb', 'March']}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# Convert key-values list to flat dictionary
+x=list(test_dict.values())
+a=x[0]
+b=x[1]
+d=dict()
+for i in range(0,len(a)):
+	d[a[i]]=b[i]
+# printing result
+print("Flattened dictionary : " + str(d))
+
+#29
