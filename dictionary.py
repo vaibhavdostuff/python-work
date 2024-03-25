d891d009167e8b1ca87ffc93f27556168f4bc8a8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-25 18:52:42 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -337,3 +337,33 @@ print("Length", len(test_dict))
 print(test_dict)
 
 #20
+# Python code demonstrate the working of sorted() and itemgetter
+
+# importing "operator" for implementing itemgetter
+from operator import itemgetter
+
+# Initializing list of dictionaries
+my_list = [{"name": "Nandini", "age": 20},
+           {"name": "Manjeet", "age": 20},
+           {"name": "Nikhil", "age": 19}]
+
+# using sorted and itemgetter to print list sorted by age
+print("The list printed sorting by age: ")
+print(sorted(my_list, key=itemgetter('age')))
+
+print("\r")
+
+# using sorted and itemgetter to print
+# list sorted by both age and name
+# notice that "Manjeet" now comes before "Nandini"
+print("The list printed sorting by age and name: ")
+print(sorted(my_list, key=itemgetter('age', 'name')))
+
+print("\r")
+
+# using sorted and itemgetter to print list
+# sorted by age in descending order
+print("The list printed sorting by age in descending order: ")
+print(sorted(my_list, key=itemgetter('age'), reverse=True))
+
+#21
