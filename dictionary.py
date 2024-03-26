0bbba51c6c5c284b5036a019f6b643fb234e7cd2
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-26 20:07:49 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -541,3 +541,18 @@ for i in range(len(test_dict['month'])):
 print("Flattened dictionary : " + str(res))
 
 #31
+# Python code to demonstrate
+# insertion of items in beginning of ordered dict
+from collections import OrderedDict
+
+# Initialising ordered_dict
+iniordered_dict = OrderedDict([('akshat', '1'), ('nikhil', '2')])
+
+# Inserting items in starting of dict
+iniordered_dict.update({'manjeet': '3'})
+iniordered_dict.move_to_end('manjeet', last=False)
+
+# Printing result
+print("Resultant Dictionary : "+str(iniordered_dict))
+
+#32
