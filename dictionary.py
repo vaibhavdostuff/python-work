97be13813ff804d0f19fc68d90919caa8e294689
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-26 20:09:43 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -625,3 +625,18 @@ if __name__ == "__main__":
 	print (checkOrder(input,pattern)) 
 
 #35
+def check_order(string, pattern):
+	i, j = 0, 0
+	for char in string:
+		if char == pattern[j]:
+			j += 1
+		if j == len(pattern):
+			return True
+		i += 1
+
+	return False
+string = 'engineers rock'
+pattern = 'er'
+print(check_order(string, pattern))
+
+#36
