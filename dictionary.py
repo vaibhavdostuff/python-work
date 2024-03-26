75a32451258b9ea30110c78c4be26e84cc658a99
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-26 20:11:07 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -686,3 +686,21 @@ if __name__ == "__main__":
 	winner(input) 
 
 #37
+from collections import Counter 
+
+votes =['john','johnny','jackie','johnny','john','jackie',
+	'jamie','jamie','john','johnny','jamie','johnny','john'] 
+
+#Count the votes for persons and stores in the dictionary
+vote_count=Counter(votes)
+
+#Find the maximum number of votes
+max_votes=max(vote_count.values())
+
+#Search for people having maximum votes and store in a list
+lst=[i for i in vote_count.keys() if vote_count[i]==max_votes]
+
+#Sort the list and print lexicographical smallest name
+print(sorted(lst)[0])
+
+#38
