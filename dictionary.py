23b0c9c1ed632d2ba168eb028ac7a1a83f726c30
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-26 20:09:21 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -591,3 +591,37 @@ while ini_dict:
 print("Resultant Dictionary :" + str(iniordered_dict))
 
 #34
+# Function to check if string follows order of 
+# characters defined by a pattern 
+from collections import OrderedDict 
+
+def checkOrder(input, pattern): 
+	
+	# create empty OrderedDict 
+	# output will be like {'a': None,'b': None, 'c': None} 
+	dict = OrderedDict.fromkeys(input) 
+
+	# traverse generated OrderedDict parallel with 
+	# pattern string to check if order of characters 
+	# are same or not 
+	ptrlen = 0
+	for key,value in dict.items(): 
+		if (key == pattern[ptrlen]): 
+			ptrlen = ptrlen + 1
+		
+		# check if we have traverse complete 
+		# pattern string 
+		if (ptrlen == (len(pattern))): 
+			return 'true'
+
+	# if we come out from for loop that means 
+	# order was mismatched 
+	return 'false'
+
+# Driver program 
+if __name__ == "__main__": 
+	input = 'engineers rock'
+	pattern = 'er'
+	print (checkOrder(input,pattern)) 
+
+#35
