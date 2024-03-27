8661dc5b53af1bc3fea870b2e0b47dcbba92ba3e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:26:15 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -847,3 +847,39 @@ dict1 = OrderedDict(sorted(dict.items()))
 print(dict1)
 
 #46
+# function calling
+def dictionairy():
+
+	# Declaring the hash function
+	key_value = {}
+
+# Initialize value
+	key_value[2] = 56
+	key_value[1] = 2
+	key_value[5] = 12
+	key_value[4] = 24
+	key_value[6] = 18
+	key_value[3] = 323
+	
+	print("key_value",key_value)
+
+	print("Task 2:-\nKeys and Values sorted in",
+		"alphabetical order by the key ")
+	
+
+	# sorted(key_value) returns a sorted list
+	# of the Dictionary’s keys.
+	for i in sorted(key_value):
+		print((i, key_value[i]), end=" ")
+
+
+def main():
+		# function calling
+	dictionairy()
+
+
+# main function calling
+if __name__ == "__main__":
+	main()
+
+#47
