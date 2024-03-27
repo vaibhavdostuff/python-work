e0f729bf180fef1dbb0a84bba94e60ab1e61daef
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:22:24 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -704,3 +704,20 @@ lst=[i for i in vote_count.keys() if vote_count[i]==max_votes]
 print(sorted(lst)[0])
 
 #38
+# Python3 code to demonstrate working of 
+# Append Dictionary Keys and Values ( In order ) in dictionary
+# Using values() + keys() + list()
+
+# initializing dictionary
+test_dict = {"Gfg" : 1, "is" : 3, "Best" : 2}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# + operator is used to perform adding keys and values
+res = list(test_dict.keys()) + list(test_dict.values())
+
+# printing result 
+print("The ordered keys and values : " + str(res)) 
+
+#39
