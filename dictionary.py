24632567a7bddfe65357ae1bbe4d913e0dd1be7d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:25:44 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -805,3 +805,36 @@ sorted_dict = {i: myDict[i] for i in myKeys}
 print(sorted_dict)
 
 #44
+# Function calling
+def dictionary():
+	# Declare hash function
+	key_value = {}
+
+# Initializing value
+	key_value[2] = 56
+	key_value[1] = 2
+	key_value[5] = 12
+	key_value[4] = 24
+	key_value[6] = 18
+	key_value[3] = 323
+
+	print("Task 1:-\n")
+
+	print("key_value", key_value)
+
+	# iterkeys() returns an iterator over the
+	# dictionary’s keys.
+	for i in sorted(key_value.keys()):
+		print(i, end=" ")
+
+
+def main():
+	# function calling
+	dictionary()
+
+
+# Main function calling
+if __name__ == "__main__":
+	main()
+
+#45
