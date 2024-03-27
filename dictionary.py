0ce094b83b636f5fb695980c42b5bb7bc14ec395
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:27:09 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -918,3 +918,19 @@ if __name__ == "__main__":
 	main()
 
 #48
+# Creates a sorted dictionary (sorted by key)
+from collections import OrderedDict
+import numpy as np
+
+dict = {'ravi': 10, 'rajnish': 9,
+		'sanjeev': 15, 'yash': 2, 'suraj': 32}
+print(dict)
+
+keys = list(dict.keys())
+values = list(dict.values())
+sorted_value_index = np.argsort(values)
+sorted_dict = {keys[i]: values[i] for i in sorted_value_index}
+
+print(sorted_dict)
+
+#49
