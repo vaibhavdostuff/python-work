c4424a5f1934c6c7d4afddda45d3b6016c438778
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:23:18 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -721,3 +721,21 @@ res = list(test_dict.keys()) + list(test_dict.values())
 print("The ordered keys and values : " + str(res)) 
 
 #39
+# Python3 code to demonstrate working of 
+# Append Dictionary Keys and Values ( In order ) in dictionary
+# Using chain() + keys() + values()
+from itertools import chain
+
+# initializing dictionary
+test_dict = {"Gfg" : 1, "is" : 3, "Best" : 2}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# chain() is used for concatenation
+res = list(chain(test_dict.keys(), test_dict.values()))
+
+# printing result 
+print("The ordered keys and values : " + str(res)) 
+
+#40
