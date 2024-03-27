937c3928b62976032239cd45753c27f843d568bc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-27 23:25:07 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -795,3 +795,13 @@ res = keys + values
 print("The ordered keys and values : " + str(res)) 
 
 #43
+myDict = {'ravi': 10, 'rajnish': 9,
+		'sanjeev': 15, 'yash': 2, 'suraj': 32}
+
+myKeys = list(myDict.keys())
+myKeys.sort()
+sorted_dict = {i: myDict[i] for i in myKeys}
+
+print(sorted_dict)
+
+#44
