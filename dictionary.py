1acce5b5b76ef76c8dd4ddac459e681b8f4c1cac
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-28 17:54:55 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1053,4 +1053,14 @@ else:
 	print("Key not found")
 
 #56
-	
+country_code = {'India' : '0091',
+				'Australia' : '0025',
+				'Nepal' : '00977'}
+
+# search dictionary for country code of India
+print(country_code.get('India', 'Not Found'))
+
+# search dictionary for country code of Japan
+print(country_code.get('Japan', 'Not Found'))
+
+#57
