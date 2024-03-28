4ab883a3d63e01b429f4b47c240bea935c54185a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-28 17:54:34 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1046,3 +1046,11 @@ print ("The value associated with 'c' is : ")
 print (d['c'])
 
 #55
+ele = {'a': 5, 'c': 8, 'e': 2}
+if "q" in ele:
+	print(ele["d"])
+else:
+	print("Key not found")
+
+#56
+	
