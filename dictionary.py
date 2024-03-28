d0e6b2304d45ad160b1acec0147a6c48aeb26af8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-28 17:52:36 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -956,3 +956,23 @@ for key in sorted(test_dict):
 print("The sorted dictionary : " + str(res)) 
 
 #50
+# Python3 code to demonstrate working of 
+# Sort Dictionary key and values List
+# Using dictionary comprehension + sorted()
+
+# initializing dictionary
+test_dict = {'gfg': [7, 6, 3], 
+			'is': [2, 10, 3], 
+			'best': [19, 4]}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# Sort Dictionary key and values List
+# Using dictionary comprehension + sorted()
+res = {key : sorted(test_dict[key]) for key in sorted(test_dict)}
+
+# printing result 
+print("The sorted dictionary : " + str(res)) 
+
+#51
