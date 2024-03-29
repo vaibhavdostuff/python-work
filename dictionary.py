779b5009c351e7b857a11116814ddeb8585ad0d4
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:10:22 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1152,3 +1152,22 @@ print(long)
 print(plc)
 
 #62
+# Creating a dictionary with multiple inputs for keys
+data = {
+	(1, "John", "Doe"): {"a": "geeks", "b": "software", "c": 75000},
+	(2, "Jane", "Smith"): {"e": 30, "f": "for", "g": 90000},
+	(3, "Bob", "Johnson"): {"h": 35, "i": "project", "j": "geeks"},
+	(4, "Alice", "Lee"): {"k": 40, "l": "marketing", "m": 100000}
+}
+
+# Accessing the values using the keys
+print(data[(1, "John", "Doe")]["a"])
+print(data[(2, "Jane", "Smith")]["f"])
+print(data[(3, "Bob", "Johnson")]["j"])
+
+data[(1, "John", "Doe")]["a"] = {"b": "marketing", "c": 75000};
+data[(3, "Bob", "Johnson")]["j"] = {"h": 35, "i": "project"};
+print(data[(1, "John", "Doe")]["a"]);
+print(data[(3, "Bob", "Johnson")]["j"]);
+
+#63
