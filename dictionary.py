c4cebef5bd7279f34d15a720c0bfcf464092e5fa
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:10:07 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1130,3 +1130,25 @@ dict[x, y, z] = x + y - z;
 print(dict)
 
 #61
+# dictionary containing longitude and latitude of places
+places = {("19.07'53.2", "72.54'51.0"):"Mumbai", \
+		("28.33'34.1", "77.06'16.6"):"Delhi"}
+
+print(places)
+print('\n')
+
+# Traversing dictionary with multi-keys and creating
+# different lists from it
+lat = []
+long = []
+plc = []
+for i in places:
+	lat.append(i[0])
+	long.append(i[1])
+	plc.append(places[i[0], i[1]])
+
+print(lat)
+print(long)
+print(plc)
+
+#62
