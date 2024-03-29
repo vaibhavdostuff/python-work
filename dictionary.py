9a2ad00aa5fe0752f499bc55ae6c657c7ad2071f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:08:50 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1064,3 +1064,17 @@ print(country_code.get('India', 'Not Found'))
 print(country_code.get('Japan', 'Not Found'))
 
 #57
+country_code = {'India' : '0091',
+				'Australia' : '0025',
+				'Nepal' : '00977'}
+
+# Set a default value for Japan
+country_code.setdefault('Japan', 'Not Present') 
+
+# search dictionary for country code of India
+print(country_code['India'])
+
+# search dictionary for country code of Japan
+print(country_code['Japan'])
+
+#58
