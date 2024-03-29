16ccfff36fc46b7c697c20d74c2fa3bfd6bd1c50
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:09:49 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1111,3 +1111,22 @@ except KeyError:
 	print('Not Found')
 
 #60
+# Python code to demonstrate a dictionary
+# with multiple inputs in a key.
+import random as rn
+
+# creating an empty dictionary
+dict = {}
+
+# Insert first triplet in dictionary
+x, y, z = 10, 20, 30
+dict[x, y, z] = x + y - z;
+
+# Insert second triplet in dictionary
+x, y, z = 5, 2, 4
+dict[x, y, z] = x + y - z;
+
+# print the dictionary
+print(dict)
+
+#61
