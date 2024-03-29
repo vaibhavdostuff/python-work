21fc6c68f7831b0f4c09f680eccfba60345880bd
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:09:28 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1100,3 +1100,14 @@ print ("The value associated with 'c' is : ",end="")
 print (defd['c'])
 
 #59
+country_code = {'India': '0091',
+				'Australia': '0025',
+				'Nepal': '00977'}
+
+try:
+	print(country_code['India'])
+	print(country_code['USA'])
+except KeyError:
+	print('Not Found')
+
+#60
