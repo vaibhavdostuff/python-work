0513617350585b65bcb35d2769759a279fddf221
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-29 18:09:10 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1078,3 +1078,25 @@ print(country_code['India'])
 print(country_code['Japan'])
 
 #58
+# Python code to demonstrate defaultdict
+import collections
+
+# declaring defaultdict
+# sets default value 'Key Not found' to absent keys
+defd = collections.defaultdict(lambda : 'Key Not found')
+
+# initializing values 
+defd['a'] = 1
+
+# initializing values 
+defd['b'] = 2
+
+# printing value 
+print ("The value associated with 'a' is : ",end="")
+print (defd['a'])
+
+# printing value associated with 'c'
+print ("The value associated with 'c' is : ",end="")
+print (defd['c'])
+
+#59
