d43009ea60e6ee1a02d7e78c9f5f0ea08dd57814
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-03-30 19:05:33 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1335,3 +1335,17 @@ if __name__ == "__main__":
 	maxAnagramSize(input)
 
 #68
+def largest_anagram_subset_size(words):
+	anagram_dict = {}
+	for word in words:
+		sorted_word = ''.join(sorted(word))
+		if sorted_word not in anagram_dict:
+			anagram_dict[sorted_word] = []
+		anagram_dict[sorted_word].append(word)
+	max_count = max([len(val) for val in anagram_dict.values()])
+	return max_count
+
+words = ['ant', 'magenta', 'magnate', 'tan', 'gnamate']
+print(largest_anagram_subset_size(words)) 
+
+#69
