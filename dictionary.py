0a771a2414a96d531b3a8ad06088fda59942ebd4
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-01 21:32:03 +0530

    dictionary python programm

--- dictionary.py
+++ dictionary.py
@@ -1558,3 +1558,19 @@ if __name__ == "__main__":
 	CountFrequency(my_list)
 
 #82
+# Python code to convert into dictionary
+
+
+def Convert(tup, di):
+	for a, b in tup:
+		di.setdefault(a, []).append(b)
+	return di
+
+
+# Driver Code
+tups = [("akash", 10), ("gaurav", 12), ("anand", 14),
+		("suraj", 20), ("akhil", 25), ("ashish", 30)]
+dictionary = {}
+print(Convert(tups, dictionary))
+
+#83
