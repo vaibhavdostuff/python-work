724d4d56601ec7b81b5d0ad395c088f12b0194dd
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-03 19:06:48 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -124,3 +124,15 @@ def search(arr, x):
 	return -1
 
 #5
+# This is similar to the above one, with the only difference 
+# being that it is using the recursive approach instead of iterative.
+
+
+def search(arr, curr_index, key):
+	if curr_index == -1:
+		return -1
+	if arr[curr_index] == key:
+		return curr_index
+	return search(arr, curr_index-1, key)
+
+#6
