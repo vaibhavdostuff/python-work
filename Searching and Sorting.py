40cb953ef11f8699306f1c674550ca879661ee76
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-05 23:50:59 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -507,3 +507,21 @@ print("The following is a Topological Sort of the given graph")
 g.nonRecursiveTopologicalSort()
 
 #17
+import bisect
+
+def binary_search(arr, val, start, end):
+	idx = bisect.bisect_left(arr[start:end+1], val)
+	return start + idx
+
+def insertion_sort(arr):
+	for i in range(1, len(arr)):
+		val = arr[i]
+		j = binary_search(arr, val, 0, i-1)
+		arr = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+	return arr
+
+print("Sorted array:")
+print(insertion_sort([37, 23, 0, 17, 12, 72, 31,
+					46, 100, 88, 54]))
+
+#18
