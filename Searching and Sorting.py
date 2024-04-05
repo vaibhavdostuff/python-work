43dde612a48a855521940d8b6e34e20afaf882ae
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-05 23:49:56 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -437,3 +437,43 @@ for i in range(n):
 	print(arr[i]),
 
 #16
+#Python program to print topological sorting of a DAG
+from collections import defaultdict
+
+#Class to represent a graph
+class Graph:
+	def __init__(self,vertices):
+		self.graph = defaultdict(list) #dictionary containing adjacency List
+		self.V = vertices #No. of vertices
+
+	# function to add an edge to graph
+	def addEdge(self,u,v):
+		self.graph[u].append(v)
+
+	# neighbors generator given key
+	def neighbor_gen(self,v):
+		for k in self.graph[v]:
+			yield k
+	
+	# non recursive topological sort
+	def nonRecursiveTopologicalSortUtil(self, v, visited,stack):
+		
+		# working stack contains key and the corresponding current generator
+		working_stack = [(v,self.neighbor_gen(v))]
+		
+		while working_stack:
+			# get last element from stack
+			v, gen = working_stack.pop()
+			visited[v] = True
+			
+			# run through neighbor generator until it's empty
+			for next_neighbor in gen:
+				if not visited[next_neighbor]: # not seen before?
+					# remember current work
+					working_stack.append((v,gen))
+					# restart with new neighbor
+					working_stack.append((next_neighbor, self.neighbor_gen(next_neighbor)))
+					break
+			else:
+				# no already-visited neighbor (or no more of them)
+				stack.append(v)
