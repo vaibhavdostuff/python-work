24f9b4331b3c89622ed613bc053fb266b60099ab
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-06 17:39:55 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -776,3 +776,19 @@ for i in range(len(a)):
 	print ("%d" %a[i]),
 
 #24
+import random
+
+# Sorts array a[0..n-1] using Bogo sort
+def bogoSort(a):
+	n = len(a)
+	while not sorted(a) == a:
+		random.shuffle(a)
+
+# Driver code to test above
+a = [3, 2, 4, 1, 0, 5]
+bogoSort(a)
+print("Sorted array:")
+for i in range(len(a)):
+	print ("%d" %a[i]),
+
+#25
