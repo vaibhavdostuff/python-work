4715ac16eeaf1fb84cd907e51d1bba4ca91d3032
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-06 17:37:26 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -723,3 +723,24 @@ for i in range(len(a)):
 	print ("%d" %a[i]),
 
 #22
+def oddEvenSort(arr, n): 
+	isSorted = 0
+	while isSorted == 0: 
+		isSorted = 1
+		arr[1::2], arr[2::2] = sorted(arr[1::2]), sorted(arr[2::2])
+		
+		for i in range(0, n-1, 2): 
+			if arr[i] > arr[i+1]: 
+				arr[i], arr[i+1] = arr[i+1], arr[i] 
+				isSorted = 0
+				
+	return
+
+arr = [34, 2, 10, -9] 
+n = len(arr) 
+
+oddEvenSort(arr, n); 
+for i in range(0, n): 
+	print(arr[i], end =" ") 
+
+#23
