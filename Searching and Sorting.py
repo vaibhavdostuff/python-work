6f453fd8253132b8a20c7fcc864ff062826e1584
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-04-06 17:49:41 +0530

    searching and sortin  python programm

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -819,28 +819,3 @@ def cycleSort(array):
 
         # Rotate the rest of the cycle.
         while pos != cycleStart:
-
-            # Find where to put the item.
-            pos = cycleStart
-            for i in range(cycleStart + 1, len(array)):
-                if array[i] < item:
-                    pos += 1
-
-            # Put the item there or right after any duplicates.
-            while item == array[pos]:
-                pos += 1
-            array[pos], item = item, array[pos]
-            writes += 1
-
-    return writes
-
-# Driver code
-arr = [1, 8, 3, 9, 10, 10, 2, 4]
-n = len(arr)
-cycleSort(arr)
-
-print("After sort : ")
-for i in range(0, n):
-    print(arr[i], end=' ')
-
-#26
