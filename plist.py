78b8cb47cda51a70c41fb5fbe1bafaeb9266ec0a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-11-05 16:46:01 +0530

    python

--- plist.py
+++ plist.py
@@ -1,20 +1,12 @@
 #1
+
 list1 = [2, 7, 5, 64, 14]
 for a, i in enumerate(list1):
 	if i % 2 == 0:
 		print(i, end=" ")
 
-#2
-list1 = [2, 7, 5, 64, 14]
-for i in list1:
-	if (i % 2 != 0):
-		pass
-	else:
-		print(i, end=" ")
-
-
-#3
-# Python code To print all even numbers
+#2 
+# To print all even numbers
 # in a given list using numpy array
 import numpy as np
 
@@ -27,22 +19,20 @@ even_num = li[li % 2 == 0]
 print(even_num)
 
 #4
-#python program to print all even no's in a list
-#defining list with even and odd numbers 
+
+# to print all even no's in a list
+# defining list with even and odd numbers 
 list1=[39,28,19,45,33,74,56] 
 #traversing list using for loop
 for element in list1:
-if not element&1: #condition to check even or not
-	print(element,end=' ')
-
+ if not element&1: #condition to check even or not
+	 print(element,end=' ')
 
 #5
-# Python program to print all even no's in a list
 
 
-# Defining list with even and odd numbers
 # Initializing list 
-list1=[39,28,19,45,33,74,56]
+list1=[19,28,39,15,43,74,66]
 
 # Traversing list using for loop
 for element in list1:
@@ -52,6 +42,7 @@ for element in list1:
 		print(element,end=' ')
 
 #6
+
 import numpy as np
 
 # given list
@@ -67,6 +58,7 @@ even_num = arr[np.where(arr % 2 == 0)]
 print(even_num)
 
 #7
+
 # Using the filterfalse function from the itertools module
 from itertools import filterfalse
 
@@ -81,7 +73,8 @@ for num in even_numbers:
 	print(num, end=" ")
 
 #8
-# Python program to print odd Numbers in a List
+
+# to print odd Numbers in a List
 
 # list of numbers
 list1 = [10, 21, 4, 45, 66, 93]
@@ -91,10 +84,11 @@ for num in list1:
 
 	# checking condition
 	if num % 2 != 0:
-	print(num, end=" ")
+	 print(num, end=" ")
 
 #9
-# Python program to print odd Numbers in a List
+
+# to print odd Numbers in a List
 
 # list of numbers
 list1 = [10, 21, 4, 45, 66, 93]
@@ -111,7 +105,8 @@ while(i < len(list1)):
 	i += 1
 
 #10
-# Python program to print odd Numbers in a List
+
+# to print odd Numbers in a List
 
 # list of numbers
 list1 = [10, 21, 4, 45, 66, 93]
@@ -121,34 +116,37 @@ only_odd = [num for num in list1 if num % 2 == 1]
 print(only_odd)
 
 #11
+ 
 #List of numbers
 list1 = [9,5,4,7,2]
 
 for ele in list1:
-if ele & 1: #Checking the element odd or not
-	print(ele,end=" ") 
+	if ele & 1: #Checking the element odd or not
+	 print(ele,end=" ") 
 
 
 #12
-#List of numbers
+
 list1 = [9,5,4,7,2]
 
 for ele in list1:
-if ele | 1==ele: #Checking the element odd or not
-	print(ele,end=" ") 
+	if ele | 1==ele: #Checking the element odd or not
+		print(ele,end=" ") 
 
 #13
+
 def is_odd(number):
-return number % 2 == 1
+ return number % 2 == 1
 
 def print_odd_numbers(numbers):
-odd_numbers = list(filter(is_odd, numbers))
-return odd_numbers
+ odd_numbers = list(filter(is_odd, numbers))
+ return odd_numbers
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(print_odd_numbers(numbers))
 
 #14
+
 import numpy as np
 
 # list of numbers
@@ -167,7 +165,8 @@ only_odd = arr[idx]
 print(only_odd)
 
 #15
-# Python program to print odd Numbers in a List
+
+# to print odd Numbers in a List
 from functools import reduce
 
 # list of numbers
@@ -179,7 +178,8 @@ odd_list = reduce(lambda a, b : a + [b] if b%2 else a, list1, [])
 print(odd_list)
 
 #16
-# Python program to print all even numbers in range
+
+# to print all even numbers in range
 for even_numbers in range(4,15,2):
 #here inside range function first no denotes starting,
 #second denotes end and
@@ -188,7 +188,6 @@ for even_numbers in range(4,15,2):
 
 
 #17
-# Python program to print Even Numbers in given range
 
 start = int(input("Enter the start of range: "))
 end = int(input("Enter the end of range: "))
@@ -201,7 +200,7 @@ for num in range(start, end + 1):
 		print(num, end=" ")
 
 #18
-# Python program to print Even Numbers in given range
+
 
 start = int(input("Enter the start of range: "))
 end = int(input("Enter the end of range: "))
@@ -215,6 +214,7 @@ start = start+1 if start&1 else start
 [ print( x ) for x in range(start, end + 1, 2)]
 
 #19
+
 def even(num1,num2):
 	if num1>num2:
 		return
@@ -224,9 +224,9 @@ num1=4;num2=15
 even(num1,num2)
 
 #20
-# Python code To print all even numbers 
+
 # in a given range using the lambda function
-a=4;b=15
+a=6;b=20
 li=[]
 for i in range(a,b+1):
 	li.append(i)
@@ -235,25 +235,29 @@ even_num = list(filter(lambda x: (x%2==0),li))
 print(even_num)
 
 #21
+
 x=[i for i in range(4,15+1) if i%2==0]
 print(*x)
 
 #22
+
 a=4;b=15;l=[]
 for i in range(a,b+1):
-l.append(i)
-print([a for j,a in enumerate(l) if a%2==0])
+ l.append(i)
+ print([a for j,a in enumerate(l) if a%2==0])
 
 #23
+
 a=4;b=15
 for i in range(a,b+1):
-if i%2!=0:
-	pass
+	if i%2!=0:
+	 pass
 else:
 	print(i,end=" ")
 
 #24
-# Python code To print all even numbers
+
+# To print all even numbers
 # in a given range using numpy array
 import numpy as np
 
@@ -266,7 +270,8 @@ even_num = li[li%2==0];
 print(even_num)
 
 #25
-# Python program to print odd Numbers in given range
+
+# to print odd Numbers in given range
 
 start, end = 4, 19
 
@@ -278,7 +283,8 @@ for num in range(start, end + 1):
 		print(num, end = " ")
 
 #26
-# Python program to print Even Numbers in given range
+
+# to print Even Numbers in given range
 
 start = int(input("Enter the start of range:"))
 end = int(input("Enter the end of range:"))
@@ -291,9 +297,9 @@ for num in range(start, end + 1):
 		print(num)
 
 #27
-# Uncomment the below two lines for taking the User Inputs
-#start = int(input("Enter the start of range: "))
-#end = int(input("Enter the end of range: "))
+
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
 
 # Range declaration
 start = 5
@@ -309,7 +315,8 @@ else:
 		print(num, end=" ")
 
 #28
-# Python program to print Even Numbers in given range
+
+# to print Even Numbers in given range
 
 start = int(input("Enter the start of range: "))
 end = int(input("Enter the end of range: "))
@@ -321,7 +328,8 @@ for num in even_list:
 	print(num, end = " ")
 
 #29
-# Python code To print all odd numbers 
+
+# To print all odd numbers 
 # in a given range using the lambda function
 a=3;b=11
 li=[]
@@ -332,18 +340,21 @@ odd_num = list(filter(lambda x: (x%2!=0),li))
 print(odd_num)
 
 #30
+
 def odd(num1,num2):
 	if num1>num2:
 		return
 	if num1&1:
-	print(num1,end=" ")
-	return odd(num1+2,num2)
+		print(num1,end=" ")
+		return odd(num1+2,num2)
 	else:
-	return odd(num1+1,num2)
+		return odd(num1+1,num2)
 num1=5;num2=15
 odd(num1,num2)
 
+
 #31
+
 x = [i for i in range(4,15+1) if i%2!=0]
 print(*x)
 
