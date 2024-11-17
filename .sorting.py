dcf1bf0d6b40a4f670843c5e7c958af2aa81529a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-11-17 17:24:02 +0530

    python

--- .sorting.py
+++ .sorting.py
@@ -11,11 +11,13 @@ list.sort(reverse=True)
 print(list)
 
 #3
+
 words = ["apple", "mango" , "banana" , "orange" , "grape"]
 words.sort(key=len)
 print("Sorted by Length:", words)
 
 #4
+
 words = ["apple", "mango", "banana", "orange", "grape"]
 words.sort(key=len, reverse=True)
 print("Sorted by Length (in reverse):", words)
@@ -31,5 +33,3 @@ print("Sorted by Age:", people)
 people = [("vaibhav", 25), ("sriansh", 30), ("aakansha", 22), ("priyanka", 28)]
 people.sort(key=lambda x: x[1], reverse=True)
 print("Sorted by Age:", people)
-
-
